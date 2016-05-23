b03efdad23e4c1690941418fa0e7f9be1a211b91
Alex Crichton <alex@alexcrichton.com>
2016-05-23 09:29:32 -0700

    Add an executor which limits recursion

--- src/executor.rs
+++ src/executor.rs
@@ -1,6 +1,7 @@
-use util;
+use std::cell::{Cell, RefCell};
+use std::sync::Arc;
 
-pub trait Executor {
+pub trait Executor: Send + Sync + 'static {
     fn execute<F>(&self, f: F)
         where F: FnOnce() + Send + 'static,
               Self: Sized
@@ -11,7 +12,13 @@ pub trait Executor {
     fn execute_boxed(&self, f: Box<ExecuteCallback>);
 }
 
-impl<T: Executor + ?Sized> Executor for Box<T> {
+impl<T: Executor + ?Sized + Send + Sync + 'static> Executor for Box<T> {
+    fn execute_boxed(&self, f: Box<ExecuteCallback>) {
+        (**self).execute_boxed(f)
+    }
+}
+
+impl<T: Executor + ?Sized + Send + Sync + 'static> Executor for Arc<T> {
     fn execute_boxed(&self, f: Box<ExecuteCallback>) {
         (**self).execute_boxed(f)
     }
@@ -27,9 +34,9 @@ impl<F: FnOnce() + Send + 'static> ExecuteCallback for F {
     }
 }
 
-pub struct DefaultExecutor;
+pub struct Default;
 
-impl Executor for DefaultExecutor {
+impl Executor for Default {
     fn execute<F: FnOnce() + Send + 'static>(&self, f: F) {
         f()
     }
@@ -39,16 +46,75 @@ impl Executor for DefaultExecutor {
     }
 }
 
-pub struct CatchUnwindExecutor;
+pub struct Limited;
 
-impl Executor for CatchUnwindExecutor {
-    fn execute<F: FnOnce() + Send + 'static>(&self, f: F) {
-        let res = util::recover::<_, _, i32>(f);
-        drop(res);
+thread_local!(static LIMITED: LimitState = LimitState::new());
+
+const LIMIT: usize = 100;
+
+struct LimitState {
+    count: Cell<usize>,
+    deferred: RefCell<Vec<Box<ExecuteCallback>>>,
+}
+
+impl Executor for Limited {
+    fn execute_boxed(&self, f: Box<ExecuteCallback>) {
+        LIMITED.with(|state| state.execute(f))
+    }
+}
+
+impl LimitState {
+    fn new() -> LimitState {
+        LimitState {
+            count: Cell::new(0),
+            deferred: RefCell::new(Vec::new()),
+        }
     }
 
-    fn execute_boxed(&self, f: Box<ExecuteCallback>) {
-        let res = util::recover::<_, _, i32>(|| f.call());
-        drop(res);
+    fn execute(&self, cb: Box<ExecuteCallback>) {
+        match self.count.get() {
+            0 => {
+                self.count.set(1);
+                let mut cb = Some(cb);
+                while let Some(f) = cb {
+                    f.call();
+                    cb = self.deferred.borrow_mut().pop();
+                }
+                self.count.set(0);
+            }
+            n if n < LIMIT => {
+                self.count.set(n + 1);
+                cb.call();
+                self.count.set(n);
+            }
+            _ => self.deferred.borrow_mut().push(cb),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+    use std::sync::atomic::{AtomicUsize, Ordering};
+
+    use super::{Executor, Limited};
+
+    #[test]
+    fn limited() {
+        fn doit(ex: Arc<Executor>, hits: Arc<AtomicUsize>, i: usize) {
+            if i == 0 {
+                return
+            }
+            hits.fetch_add(1, Ordering::SeqCst);
+            let ex2 = ex.clone();
+            ex.execute(move || {
+                doit(ex2, hits, i - 1);
+            })
+        }
+
+        let n = 1_000_000;
+        let hits = Arc::new(AtomicUsize::new(0));
+        doit(Arc::new(Limited), hits.clone(), n);
+        assert_eq!(hits.load(Ordering::SeqCst), n);
     }
 }
