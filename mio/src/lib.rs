d3d126a5f23fdb76d54b7ff3371dade4d8e35d0f
Alex Crichton <alex@alexcrichton.com>
2016-07-11 23:09:05 -0700

    Few style tweaks here and there

--- mio/src/lib.rs
+++ mio/src/lib.rs
@@ -5,9 +5,16 @@ extern crate slab;
 #[macro_use]
 extern crate scoped_tls;
 
+use std::io;
+use futures::Future;
+use futures::stream::Stream;
+
 mod readiness_stream;
 mod event_loop;
 mod tcp;
+
+pub type IoFuture<T> = Future<Item=T, Error=io::Error>;
+pub type IoStream<T> = Stream<Item=T, Error=io::Error>;
 
 pub use event_loop::{Loop, LoopHandle, Direction};
 pub use readiness_stream::{ReadinessStream, ReadinessPair};
--- mio/src/tcp.rs
+++ mio/src/tcp.rs
@@ -1,13 +1,11 @@
-use super::event_loop::LoopHandle;
-use super::readiness_stream::ReadinessPair;
-
-use mio;
-
 use std::io::{self, ErrorKind};
 use std::net::SocketAddr;
 
-use futures::{self, Future, IntoFuture};
 use futures::stream::Stream;
+use futures::{self, Future, IntoFuture};
+use mio;
+
+use {IoFuture, IoStream, ReadinessPair, LoopHandle};
 
 pub struct TcpListener {
     loop_handle: LoopHandle,
@@ -16,7 +14,7 @@ pub struct TcpListener {
 
 impl TcpListener {
     pub fn new(handle: LoopHandle, addr: &SocketAddr)
-               -> Box<Future<Item=TcpListener, Error=io::Error>>
+               -> Box<IoFuture<TcpListener>>
     {
         let cloned_handle = handle.clone();
         mio::tcp::TcpListener::bind(addr)
@@ -30,17 +28,17 @@ impl TcpListener {
         self.inner.source.local_addr()
     }
 
-    pub fn incoming(self) -> Box<Stream<Item=(TcpStream, SocketAddr), Error=io::Error>> {
+    pub fn incoming(self) -> Box<IoStream<(TcpStream, SocketAddr)>> {
         let TcpListener { loop_handle, inner } = self;
-        let clone_handle = move || loop_handle.clone();
         let source = inner.source;
 
         inner.ready_read
-            .and_then(move |_| source.accept())
+            .and_then(move |()| source.accept())
             .filter_map(|i| i)
             .and_then(move |(tcp, addr)| {
-                ReadinessPair::new(clone_handle(), tcp)
-                    .map(move |pair| (pair, addr))
+                ReadinessPair::new(loop_handle.clone(), tcp).map(move |pair| {
+                    (pair, addr)
+                })
             }).boxed()
     }
 }
@@ -49,34 +47,35 @@ pub type TcpStream = ReadinessPair<mio::tcp::TcpStream>;
 
 pub fn tcp_connect(handle: LoopHandle,
                    addr: &SocketAddr)
-                   -> Box<Future<Item = TcpStream, Error = io::Error>> {
-    match mio::tcp::TcpStream::connect(addr) {
-        Ok(tcp) => {
-            ReadinessPair::new(handle, tcp)
-                .and_then(|ReadinessPair { source, ready_read, ready_write }| {
-                    let source_for_skip = source.clone(); // TODO: find a better way to do this
-                    let connected = ready_write.skip_while(move |_| {
-                        match source_for_skip.take_socket_error() {
-                            Ok(()) => Ok(false),
-                            Err(e) => {
-                                if e.kind() == ErrorKind::WouldBlock {
-                                    Ok(true)
-                                } else {
-                                    Err(e)
-                                }
-                            }
-                        }
-                    });
-                    let connected = connected.into_future();
-                    connected.map(move |(_, stream)| {
-                        ReadinessPair {
-                            source: source,
-                            ready_read: ready_read,
-                            ready_write: stream.into_inner()
-                        }
-                    })
-                }).boxed()
-        }
-        Err(e) => futures::failed(e).boxed(),
-    }
+                   -> Box<IoFuture<TcpStream>> {
+    let stream = match mio::tcp::TcpStream::connect(addr) {
+        Ok(tcp) => tcp,
+        Err(e) => return futures::failed(e).boxed(),
+    };
+
+    // Once we've connected, wait for the stream to be writable as that's when
+    // the actual connection has been initiated. Once we're writable we check
+    // for `take_socket_error` to see if the connect actually hit an error or
+    // not.
+    //
+    // If all that succeeded then we ship everything on up.
+    ReadinessPair::new(handle, stream).and_then(|pair| {
+        let ReadinessPair { source, ready_read, ready_write } = pair;
+        let source_for_skip = source.clone(); // TODO: find a better way to do this
+        let connected = ready_write.skip_while(move |&()| {
+            match source_for_skip.take_socket_error() {
+                Ok(()) => Ok(false),
+                Err(ref e) if e.kind() == ErrorKind::WouldBlock => Ok(true),
+                Err(e) => Err(e),
+            }
+        });
+        let connected = connected.into_future();
+        connected.map(move |(_, stream)| {
+            ReadinessPair {
+                source: source,
+                ready_read: ready_read,
+                ready_write: stream.into_inner()
+            }
+        })
+    }).boxed()
 }
