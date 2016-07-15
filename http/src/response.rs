27a59d321cdd700461a1b24b4af654efbbbe6d7b
Alex Crichton <alex@alexcrichton.com>
2016-07-15 16:38:55 -0700

    Few more serialization optimizations

--- http/src/response.rs
+++ http/src/response.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::fmt::{self, Write};
 
 use time;
 
@@ -30,19 +30,47 @@ impl Response {
 
 impl Serialize for Response {
     fn serialize(&self, buf: &mut Vec<u8>) {
-        write!(buf, "\
+        write!(FastWrite(buf), "\
             HTTP/1.1 200 OK\r\n\
             Server: Example\r\n\
             Content-Length: {}\r\n\
             Date: {}\r\n\
         ", self.response.len(), time::now().rfc822()).unwrap();
         for &(ref k, ref v) in &self.headers {
-            buf.extend_from_slice(k.as_bytes());
-            buf.extend_from_slice(b": ");
-            buf.extend_from_slice(v.as_bytes());
-            buf.extend_from_slice(b"\r\n");
+            extend(buf, k.as_bytes());
+            extend(buf, b": ");
+            extend(buf, v.as_bytes());
+            extend(buf, b"\r\n");
         }
-        buf.extend_from_slice(b"\r\n");
-        buf.extend_from_slice(self.response.as_bytes());
+        extend(buf, b"\r\n");
+        extend(buf, self.response.as_bytes());
     }
 }
+
+// TODO: impl fmt::Write for Vec<u8>
+//
+// Right now `write!` on `Vec<u8>` goes through io::Write and is not super
+// speedy, so inline a less-crufty implementation here which doesn't go through
+// io::Error.
+struct FastWrite<'a>(&'a mut Vec<u8>);
+
+impl<'a> fmt::Write for FastWrite<'a> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        extend(self.0, s.as_bytes());
+        Ok(())
+    }
+}
+
+// TODO: why doesn't extend_from_slice optimize to this?
+fn extend(dst: &mut Vec<u8>, data: &[u8]) {
+    use std::ptr;
+    dst.reserve(data.len());
+    let prev = dst.len();
+    unsafe {
+        ptr::copy_nonoverlapping(data.as_ptr(),
+                                 dst.as_mut_ptr().offset(prev as isize),
+                                 data.len());
+        dst.set_len(prev + data.len());
+    }
+}
+
