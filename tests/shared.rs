b6b05eb81681646950cc9bd001bcf5d2df736033
Liran Ringel <liranringel@gmail.com>
2016-11-01 01:34:45 +0200

    Added many_threads test

--- tests/shared.rs
+++ tests/shared.rs
@@ -7,7 +7,6 @@ use futures::{oneshot, Future};
 fn two_threads() {
     let (tx, rx) = oneshot::<u32>();
     let f1 = rx.shared();
-    println!("after shared()");
     let f2 = f1.clone();
 
     let (tx1, rx1) = oneshot::<()>();
@@ -15,21 +14,38 @@ fn two_threads() {
 
     thread::spawn(move || {
         assert!(*f1.wait().unwrap() == 3);
-        println!("after f1.wait()");
         tx1.complete(());
     });
     thread::spawn(move || {
         assert!(*f2.wait().unwrap() == 3);
-        println!("after f2.wait()");
         tx2.complete(());
     });
 
     thread::spawn(|| {
-        println!("after spawn");
         tx.complete(3);
     });
 
     rx1.wait().unwrap();
     rx2.wait().unwrap();
-    println!("done!");
+}
+
+
+#[test]
+fn many_threads() {
+    let (tx, rx) = oneshot::<u32>();
+    let f = rx.shared();
+    let mut cloned_futures_waited_oneshots = vec![];
+    for _ in 0..10000 {
+        let cloned_future = f.clone();
+        let (tx2, rx2) = oneshot::<()>();
+        cloned_futures_waited_oneshots.push(rx2);
+        thread::spawn(move || {
+            assert!(*cloned_future.wait().unwrap() == 3);
+            tx2.complete(());
+        });
+    }
+    tx.complete(3);
+    for f in cloned_futures_waited_oneshots {
+        f.wait().unwrap();
+    }
 }
