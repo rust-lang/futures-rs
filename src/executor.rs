f3721bfa8feda4b7def0593397a344784f4233f9
Alex Crichton <alex@alexcrichton.com>
2016-11-04 15:32:19 -0700

    Unpark is a `task` module piece

--- src/executor.rs
+++ src/executor.rs
@@ -4,4 +4,4 @@
 //! which is needed when building *executors* (places where futures can run).
 // TODO: more dox
 
-pub use task_impl::{Spawn, spawn, Unpark, Executor, Run};
+pub use task_impl::{Spawn, spawn, Executor, Run};
