a2cfbeef532dd6acf8dd51c8863faf15b88b87c6
Liran Ringel <liranringel@gmail.com>
2016-11-05 19:32:40 +0200

    Use Lock instead of Mutex

--- src/shared.rs
+++ src/shared.rs
@@ -1,11 +1,15 @@
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::ops::Deref;
 use std::sync::RwLock;
 use std::sync::mpsc::{channel, Receiver, Sender};
 use {Future, Poll, Async};
 use task::{self, Task};
+use lock::Lock;
 
+
+/// A wrapped item of the original future.
+/// It is clonable and implements Deref for ease of use.
 #[derive(Debug)]
 pub struct SharedItem<T> {
     item: Arc<T>,
@@ -17,6 +21,16 @@ impl<T> Clone for SharedItem<T> {
     }
 }
 
+impl<T> Deref for SharedItem<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.item.as_ref()
+    }
+}
+
+/// A wrapped error of the original future.
+/// It is clonable and implements Deref for ease of use.
 #[derive(Debug)]
 pub struct SharedError<E> {
     error: Arc<E>,
@@ -25,6 +39,14 @@ pub struct SharedError<E> {
 impl<T> Clone for SharedError<T> {
     fn clone(&self) -> Self {
         SharedError { error: self.error.clone() }
+    }
+}
+
+impl<E> Deref for SharedError<E> {
+    type Target = E;
+
+    fn deref(&self) -> &E {
+        &self.error.as_ref()
     }
 }
 
@@ -40,12 +62,13 @@ impl<E> SharedError<E> {
     }
 }
 
+/// The data that has to be synced to implement `Shared`, in order to satisfy the `Future` trait's constraints.
 struct SyncedInner<F>
     where F: Future
 {
-    original_future: F,
-    result: Option<Result<Async<SharedItem<F::Item>>, SharedError<F::Error>>>,
-    tasks_receiver: Mutex<Receiver<Task>>,
+    original_future: F, // The original future
+    result: Option<Result<Async<SharedItem<F::Item>>, SharedError<F::Error>>>, // The original future result wrapped with `SharedItem`/`SharedError`
+    tasks_receiver: Lock<Receiver<Task>>, // When original future is polled and ready, unparks all the tasks in that channel
 }
 
 struct Inner<F>
@@ -73,7 +96,7 @@ pub fn new<F>(future: F) -> Shared<F>
             synced_inner: RwLock::new(SyncedInner {
                 original_future: future,
                 result: None,
-                tasks_receiver: Mutex::new(tasks_receiver),
+                tasks_receiver: Lock::new(tasks_receiver),
             }),
             tasks_unpark_started: AtomicBool::new(false),
         }),
@@ -134,7 +157,7 @@ impl<F> Future for Shared<F>
                     let ref inner = *inner_guard;
                     self.inner.tasks_unpark_started.store(true, Ordering::Relaxed);
                     match inner.tasks_receiver.try_lock() {
-                        Ok(tasks_receiver_guard) => {
+                        Some(tasks_receiver_guard) => {
                             let ref tasks_receiver = *tasks_receiver_guard;
                             loop {
                                 match tasks_receiver.try_recv() {
@@ -143,7 +166,7 @@ impl<F> Future for Shared<F>
                                 }
                             }
                         }
-                        Err(_) => {} // Other thread is unparking the tasks
+                        None => {} // Other thread is unparking the tasks
                     }
 
                     if let Some(ref result) = inner.result {
@@ -191,21 +214,5 @@ impl<F> Clone for Shared<F>
             inner: self.inner.clone(),
             tasks_sender: self.tasks_sender.clone(),
         }
-    }
-}
-
-impl<T> Deref for SharedItem<T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        &self.item.as_ref()
-    }
-}
-
-impl<E> Deref for SharedError<E> {
-    type Target = E;
-
-    fn deref(&self) -> &E {
-        &self.error.as_ref()
     }
 }
