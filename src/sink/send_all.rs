bde5bb16989fdb9b20256b4b25d18e43b40ac6dd
Paul Colomiets <paul@colomiets.name>
2016-11-19 01:45:26 +0200

    Changes `sink.send_all(stream)` to return tuple `(sink, stream)`

--- src/sink/send_all.rs
+++ src/sink/send_all.rs
@@ -7,7 +7,7 @@ use sink::Sink;
 #[must_use = "futures do nothing unless polled"]
 pub struct SendAll<T, U: Stream> {
     sink: Option<T>,
-    stream: Fuse<U>,
+    stream: Option<Fuse<U>>,
     buffered: Option<U::Item>,
 }
 
@@ -18,7 +18,7 @@ pub fn new<T, U>(sink: T, stream: U) -> SendAll<T, U>
 {
     SendAll {
         sink: Some(sink),
-        stream: stream.fuse(),
+        stream: Some(stream.fuse()),
         buffered: None,
     }
 }
@@ -32,8 +32,17 @@ impl<T, U> SendAll<T, U>
         self.sink.as_mut().take().expect("Attempted to poll SendAll after completion")
     }
 
-    fn take_sink(&mut self) -> T {
-        self.sink.take().expect("Attempted to poll SendAll after completion")
+    fn stream_mut(&mut self) -> &mut Fuse<U> {
+        self.stream.as_mut().take()
+            .expect("Attempted to poll SendAll after completion")
+    }
+
+    fn take_result(&mut self) -> (T, U) {
+        let sink = self.sink.take()
+            .expect("Attempted to poll Forward after completion");
+        let fuse = self.stream.take()
+            .expect("Attempted to poll Forward after completion");
+        return (sink, fuse.into_inner());
     }
 
     fn try_start_send(&mut self, item: U::Item) -> Poll<(), T::SinkError> {
@@ -51,10 +60,10 @@ impl<T, U> Future for SendAll<T, U>
           U: Stream<Item = T::SinkItem>,
           T::SinkError: From<U::Error>,
 {
-    type Item = T;
+    type Item = (T, U);
     type Error = T::SinkError;
 
-    fn poll(&mut self) -> Poll<T, T::SinkError> {
+    fn poll(&mut self) -> Poll<(T, U), T::SinkError> {
         // If we've got an item buffered already, we need to write it to the
         // sink before we can do anything else
         if let Some(item) = self.buffered.take() {
@@ -64,7 +73,7 @@ impl<T, U> Future for SendAll<T, U>
         loop {
             try!(self.sink_mut().poll_complete());
 
-            if let Some(item) = try_ready!(self.stream.poll()) {
+            if let Some(item) = try_ready!(self.stream_mut().poll()) {
                 try_ready!(self.try_start_send(item))
             } else {
                 // we're done pushing the stream, but want to block on flushing the
@@ -72,7 +81,7 @@ impl<T, U> Future for SendAll<T, U>
                 try_ready!(self.sink_mut().poll_complete());
 
                 // now everything's emptied, so return the sink for further use
-                return Ok(Async::Ready(self.take_sink()))
+                return Ok(Async::Ready(self.take_result()))
             }
         }
     }
