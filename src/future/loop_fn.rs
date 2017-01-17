5fccd44aacf925832bd98580532a2fa21e26f6b9
Michael Smith <michael@spinda.net>
2017-01-17 00:11:04 -0800

    Rename tail_fn -> loop_fn and add loop (see #279)

--- src/future/loop_fn.rs
+++ src/future/loop_fn.rs
@@ -1,21 +1,21 @@
-//! Definition of the `TailFn` combinator, implementing tail-recursive loops.
+//! Definition of the `LoopFn` combinator, implementing `Future` loops.
 
 use {Async, Future, IntoFuture, Poll};
 
-/// The status of a `tail_fn` loop.
-pub enum Tail<T, S> {
+/// The status of a `loop_fn` loop.
+pub enum Loop<T, S> {
     /// Indicates that the loop has completed with output `T`.
-    Done(T),
+    Break(T),
 
     /// Indicates that the loop function should be called again with input
     /// state `S`.
-    Loop(S),
+    Continue(S),
 }
 
 /// A future implementing a tail-recursive loop.
 ///
-/// Created by the `tail_fn` function.
-pub struct TailFn<A, F> {
+/// Created by the `loop_fn` function.
+pub struct LoopFn<A, F> {
     future: A,
     func: F,
 }
@@ -24,18 +24,18 @@ pub struct TailFn<A, F> {
 ///
 /// The loop function is immediately called with `initial_state` and should
 /// return a value that can be converted to a future. On successful completion,
-/// this future should output a `Tail<T, S>` to indicate the status of the
+/// this future should output a `Loop<T, S>` to indicate the status of the
 /// loop.
 ///
-/// `Tail::Done(T)` halts the loop and completes the future with output `T`.
+/// `Loop::Break(T)` halts the loop and completes the future with output `T`.
 ///
-/// `Tail::Loop(S)` reinvokes the loop function with state `S`. The returned
-/// future will be subsequently polled for a new `Tail<T, S>` value.
+/// `Loop::Continue(S)` reinvokes the loop function with state `S`. The returned
+/// future will be subsequently polled for a new `Loop<T, S>` value.
 ///
 /// # Examples
 ///
 /// ```
-/// use futures::future::{ok, tail_fn, Future, Ok, Tail};
+/// use futures::future::{ok, loop_fn, Future, Ok, Loop};
 /// use std::io::Error;
 ///
 /// struct Client {
@@ -57,44 +57,43 @@ pub struct TailFn<A, F> {
 ///     }
 /// }
 ///
-/// let ping_til_done = tail_fn(Client::new(), |client| {
+/// let ping_til_done = loop_fn(Client::new(), |client| {
 ///     client.send_ping()
 ///         .and_then(|client| client.receive_pong())
 ///         .and_then(|(client, done)| {
 ///             if done {
-///                 Ok(Tail::Done(client))
+///                 Ok(Loop::Break(client))
 ///             } else {
-///                 Ok(Tail::Loop(client))
+///                 Ok(Loop::Continue(client))
 ///             }
 ///         })
 /// });
 /// ```
-pub fn tail_fn<S, T, I, A, F>(initial_state: S, mut func: F) -> TailFn<A, F>
+pub fn loop_fn<S, T, I, A, F>(initial_state: S, mut func: F) -> LoopFn<A, F>
     where F: FnMut(S) -> I,
-          A: Future<Item = Tail<T, S>>,
+          A: Future<Item = Loop<T, S>>,
           I: IntoFuture<Future = A, Item = A::Item, Error = A::Error>
 {
-    TailFn {
+    LoopFn {
         future: func(initial_state).into_future(),
         func: func,
     }
 }
 
-impl<S, T, I, A, F> Future for TailFn<A, F>
+impl<S, T, I, A, F> Future for LoopFn<A, F>
     where F: FnMut(S) -> I,
-          A: Future<Item = Tail<T, S>>,
+          A: Future<Item = Loop<T, S>>,
           I: IntoFuture<Future = A, Item = A::Item, Error = A::Error>
 {
     type Item = T;
     type Error = A::Error;
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-        let status = try_ready!(self.future.poll());
-        match status {
-            Tail::Done(x) => Ok(Async::Ready(x)),
-            Tail::Loop(s) => {
-                self.future = (self.func)(s).into_future();
-                Ok(Async::NotReady)
+        loop {
+            let status = try_ready!(self.future.poll());
+            match status {
+                Loop::Break(x) => return Ok(Async::Ready(x)),
+                Loop::Continue(s) => self.future = (self.func)(s).into_future(),
             }
         }
     }
