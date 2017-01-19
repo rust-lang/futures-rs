e74b2946d85ceceb63495275e59bfe9f4cf9ce3a
Alex Crichton <alex@alexcrichton.com>
2017-01-19 10:29:28 -0800

    Add futures::unsync::oneshot
    Initially inspired from the `relay` crate, this commit adds a oneshot similar to
    the `futures::sync::oneshot` in functionality except it's not intended to be
    sent across threads.

---
+++ tests/unsync-oneshot.rs
@@ -0,0 +1,45 @@
+extern crate futures;
+
+use futures::Future;
+use futures::unsync::oneshot::{channel, Canceled};
+
+#[test]
+fn smoke() {
+    let (tx, rx) = channel();
+    tx.complete(33);
+    assert_eq!(rx.wait().unwrap(), 33);
+}
+
+#[test]
+fn canceled() {
+    let (_, rx) = channel::<()>();
+    assert_eq!(rx.wait().unwrap_err(), Canceled);
+}
+
+#[test]
+fn poll_cancel() {
+    let (mut tx, _) = channel::<()>();
+    assert!(tx.poll_cancel().unwrap().is_ready());
+}
+
+#[test]
+fn tx_complete_rx_unparked() {
+    let (tx, rx) = channel();
+
+    let res = rx.join(::futures::lazy(move || {
+        tx.complete(55);
+        Ok(11)
+    }));
+    assert_eq!(res.wait().unwrap(), (55, 11));
+}
+
+#[test]
+fn tx_dropped_rx_unparked() {
+    let (tx, rx) = channel::<i32>();
+
+    let res = rx.join(::futures::lazy(move || {
+        let _tx = tx;
+        Ok(11)
+    }));
+    assert_eq!(res.wait().unwrap_err(), Canceled);
+}
