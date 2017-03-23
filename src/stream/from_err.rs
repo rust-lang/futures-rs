680b74ac3e7dccd3816d4b05baa931ba354454b6
Michael Mokrysz <hi@46b.it>
2017-03-23 23:34:10 +0000

    Added a `Sink` implementation for where `stream::FromErr` also wraps a `Sink`.
    Simplifies Client type of https://github.com/sirpent-team/comms/blob/master/src/client/mod.rs#L47-L49 into Client<I, FromErr<C>>.

--- src/stream/from_err.rs
+++ src/stream/from_err.rs
@@ -1,6 +1,6 @@
 use core::marker::PhantomData;
 
-use {Stream, Poll, Async};
+use {Stream, Poll, Async, Sink, StartSend};
 
 /// A stream combinator to change the error type of a stream.
 ///
@@ -31,5 +31,22 @@ impl<S: Stream, E: From<S::Error>> Stream for FromErr<S, E> {
             other => other,
         };
         e.map_err(From::from)
+    }
+}
+
+impl<S: Stream + Sink, E: From<S::SinkError>> Sink for FromErr<S, E> {
+    type SinkItem = S::SinkItem;
+    type SinkError = E;
+
+    fn start_send(&mut self, item: Self::SinkItem) -> StartSend<Self::SinkItem, Self::SinkError> {
+        self.stream.start_send(item).map_err(|e| e.into())
+    }
+
+    fn poll_complete(&mut self) -> Poll<(), Self::SinkError> {
+        self.stream.poll_complete().map_err(|e| e.into())
+    }
+
+    fn close(&mut self) -> Poll<(), Self::SinkError> {
+        self.stream.close().map_err(|e| e.into())
     }
 }
