541f15d4c48b86b207b1b113ee175121b891acd4
Carl Lerche <me@carllerche.com>
2017-05-12 20:43:20 -0700

    ReadyQueue: Drop futures when queue is dropped

--- src/future/ready_queue.rs
+++ src/future/ready_queue.rs
@@ -16,6 +16,8 @@ use std::sync::atomic::Ordering::{Relaxed, AcqRel, Acquire, Release};
 /// they are ready.
 pub struct ReadyQueue<T> {
     inner: Arc<Inner<T>>,
+    head_all: *mut Node<T>,
+    tail_readiness: *mut Node<T>,
 }
 
 struct Inner<T> {
@@ -26,18 +28,21 @@ struct Inner<T> {
     parent: AtomicTask,
 
     // Head of the readiness queue
-    head: AtomicPtr<Node<T>>,
-
-    // Tail of readiness queue
-    tail: UnsafeCell<*mut Node<T>>,
+    head_readiness: AtomicPtr<Node<T>>,
 }
 
 struct Node<T> {
     // The future
     future: UnsafeCell<Option<T>>,
 
-    // Next pointer
-    next: AtomicPtr<Node<T>>,
+    // Next pointer for linked list tracking all active nodes
+    next_all: UnsafeCell<*mut Node<T>>,
+
+    // Previous node in linked list tracking all active nodes
+    prev_all: UnsafeCell<*mut Node<T>>,
+
+    // Next pointer in readiness queue
+    next_readiness: AtomicPtr<Node<T>>,
 
     // Atomic state, includes the ref count
     state: AtomicUsize,
@@ -58,27 +63,31 @@ const QUEUED: usize = usize::MAX - (usize::MAX >> 1);
 impl<T> ReadyQueue<T>
     where T: Future + 'static,
 {
-
     /// Constructs a new, empty `ReadyQueue`
     pub fn new() -> ReadyQueue<T> {
         let mut stub = Box::new(Node {
             future: UnsafeCell::new(None),
-            next: AtomicPtr::new(ptr::null_mut()),
+            next_all: UnsafeCell::new(ptr::null_mut()),
+            prev_all: UnsafeCell::new(ptr::null_mut()),
+            next_readiness: AtomicPtr::new(ptr::null_mut()),
             state: AtomicUsize::new(QUEUED | 1),
         });
 
         debug_assert!(stub.state.load(Relaxed) & QUEUED == QUEUED);
 
         ReadyQueue {
+            head_all: ptr::null_mut(),
+            tail_readiness: &mut *stub as *mut _,
             inner: Arc::new(Inner {
                 parent: AtomicTask::new(),
-                head: AtomicPtr::new(&mut *stub as *mut _),
-                tail: UnsafeCell::new(&mut *stub as *mut _),
+                head_readiness: AtomicPtr::new(&mut *stub as *mut _),
                 stub: stub,
             }),
         }
     }
+}
 
+impl<T> ReadyQueue<T> {
     /// Push a future into the queue.
     ///
     /// **IMPORTANT** You *must* call `poll` after pushing futures onto the
@@ -86,14 +95,101 @@ impl<T> ReadyQueue<T>
     pub fn push(&mut self, future: T) {
         let node = Box::new(Node {
             future: UnsafeCell::new(Some(future)),
-            next: AtomicPtr::new(ptr::null_mut()),
+            next_all: UnsafeCell::new(self.head_all),
+            prev_all: UnsafeCell::new(ptr::null_mut()),
+            next_readiness: AtomicPtr::new(ptr::null_mut()),
             state: AtomicUsize::new(QUEUED | 1),
         });
 
         let ptr = Box::into_raw(node);
 
+        unsafe {
+            if let Some(curr_head) = self.head_all.as_mut() {
+                *curr_head.prev_all.get() = ptr;
+            }
+        }
+
+        self.head_all = ptr;
+
         // Enqueue the node
         self.inner.enqueue(ptr);
+    }
+
+
+    /// The dequeue function from the 1024cores intrusive MPSC queue algorithm
+    fn dequeue(&mut self) -> Dequeue<T> {
+        unsafe {
+            // This is the 1024cores.net intrusive MPSC queue [1] "pop" function
+            // with the modifications mentioned at the top of the file.
+            let mut tail = self.tail_readiness;
+            let mut next = (*tail).next_readiness.load(Acquire);
+
+            if tail == self.inner.stub() {
+                if next.is_null() {
+                    return Dequeue::Empty;
+                }
+
+                self.tail_readiness = next;
+                tail = next;
+                next = (*next).next_readiness.load(Acquire);
+            }
+
+            if !next.is_null() {
+                self.tail_readiness = next;
+                debug_assert!(tail != self.inner.stub());
+                return Dequeue::Data(tail);
+            }
+
+            if self.inner.head_readiness.load(Acquire) != tail {
+                return Dequeue::Inconsistent;
+            }
+
+            // Push the stub node
+            self.inner.enqueue(self.inner.stub());
+
+            next = (*tail).next_readiness.load(Acquire);
+
+            if !next.is_null() {
+                self.tail_readiness = next;
+                return Dequeue::Data(tail);
+            }
+
+            Dequeue::Inconsistent
+        }
+    }
+
+    fn release_node(&mut self, node: &mut Node<T>) {
+        // The future is done, try to reset the queued flag. This will prevent
+        // `notify` from doing any work in the future
+        let prev = node.state.fetch_or(QUEUED, AcqRel);
+
+        // Drop the future...
+        let _ = unsafe { (*node.future.get()).take() };
+
+        // Unlink the node
+        self.unlink(node);
+
+        if prev & QUEUED == 0 {
+            // The queued flag has been set, this means we can safely drop the
+            // node. If this doesn't happen, the node was requeued in the
+            // readiness queue, so we will see it again, but next time the `&mut
+            // None` branch will be hit freeing the node.
+            unsafe { release(node) };
+        }
+    }
+
+    fn unlink(&mut self, node: &mut Node<T>) {
+        unsafe {
+            if let Some(next) = (*node.next_all.get()).as_mut() {
+                *next.prev_all.get() = *node.prev_all.get();
+            }
+
+            if let Some(prev) = (*node.prev_all.get()).as_mut() {
+                *prev.next_all.get() = *node.next_all.get();
+            } else {
+                self.head_all = *node.next_all.get();
+            }
+        }
     }
 }
 
@@ -108,7 +204,7 @@ impl<T> Stream for ReadyQueue<T>
         unsafe { self.inner.parent.park() };
 
         loop {
-            match unsafe { self.inner.dequeue() } {
+            match self.dequeue() {
                 Dequeue::Empty => return Ok(Async::NotReady),
                 Dequeue::Inconsistent => {
                     // At this point, it may be worth yielding the thread &
@@ -121,13 +217,17 @@ impl<T> Stream for ReadyQueue<T>
                     debug_assert!(node != self.inner.stub());
                     let node = unsafe { &mut *node };
 
-                    // Unset queued flag
-                    node.state.fetch_and(!QUEUED, AcqRel);
-
                     // Only try running the future if it hasn't already been
                     // completed.
                     match unsafe { &mut *node.future.get() } {
                         &mut Some(ref mut f) => {
+                            // Unset queued flag... this must be done before
+                            // polling.
+                            node.state.fetch_and(!QUEUED, AcqRel);
+
+                            // Create the notify handler.
+                            //
+                            // TODO: Attempt to avoid the Arc clone
                             let notify = self.inner.clone().into();
                             let id = node as *const _ as u64;
 
@@ -136,15 +236,18 @@ impl<T> Stream for ReadyQueue<T>
                                 f.poll()
                             });
 
-                            // Release the node handle
-                            unsafe { release(node) };
-
                             match res {
-                                Ok(Async::NotReady) => {}
-                                Ok(Async::Ready(v)) => {
-                                    return Ok(Async::Ready(Some(v)))
+                                Ok(Async::NotReady) => {
+                                    // Nothing more to do
                                 }
-                                Err(e) => return Err(e),
+                                Ok(Async::Ready(v)) => {
+                                    self.release_node(node);
+                                    return Ok(Async::Ready(Some(v)));
+                                }
+                                Err(e) => {
+                                    self.release_node(node);
+                                    return Err(e);
+                                }
                             }
                         }
                         &mut None => {
@@ -158,66 +261,34 @@ impl<T> Stream for ReadyQueue<T>
     }
 }
 
+impl<T> Drop for ReadyQueue<T> {
+    fn drop(&mut self) {
+        unsafe {
+            while let Some(node) = self.head_all.as_mut() {
+                self.release_node(node);
+            }
+        }
+    }
+}
+
 impl<T: Debug> Debug for ReadyQueue<T> {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         write!(fmt, "ReadyQueue {{ ... }}")
     }
 }
 
-impl<T> Inner<T> where T: Future {
+impl<T> Inner<T> {
     /// The enqueue function from the 1024cores intrusive MPSC queue algorithm.
     fn enqueue(&self, node: *mut Node<T>) {
         unsafe {
             debug_assert!((*node).state.load(Relaxed) & QUEUED == QUEUED);
 
             // This action does not require any coordination
-            (*node).next.store(ptr::null_mut(), Relaxed);
+            (*node).next_readiness.store(ptr::null_mut(), Relaxed);
 
-            let prev = self.head.swap(node, AcqRel);
-            (*prev).next.store(node, Release);
+            let prev = self.head_readiness.swap(node, AcqRel);
+            (*prev).next_readiness.store(node, Release);
         }
-    }
-
-    /// The dequeue function from the 1024cores intrusive MPSC queue algorithm
-    unsafe fn dequeue(&self) -> Dequeue<T> {
-        // This is the 1024cores.net intrusive MPSC queue [1] "pop" function
-        // with the modifications mentioned at the top of the file.
-        let mut tail = *self.tail.get();
-        let mut next = (*tail).next.load(Acquire);
-
-        if tail == self.stub() {
-            if next.is_null() {
-                return Dequeue::Empty;
-            }
-
-            *self.tail.get() = next;
-            tail = next;
-            next = (*next).next.load(Acquire);
-        }
-
-        if !next.is_null() {
-            *self.tail.get() = next;
-            debug_assert!(tail != self.stub());
-            return Dequeue::Data(tail);
-        }
-
-        if self.head.load(Acquire) != tail {
-            return Dequeue::Inconsistent;
-        }
-
-        // Push the stub node
-        debug_assert!(self.stub.state.load(Relaxed) & QUEUED == QUEUED);
-        self.enqueue(self.stub());
-
-        next = (*tail).next.load(Acquire);
-
-        if !next.is_null() {
-            *self.tail.get() = next;
-            debug_assert!(tail != self.stub());
-            return Dequeue::Data(tail);
-        }
-
-        Dequeue::Inconsistent
     }
 
     fn stub(&self) -> *mut Node<T> {
@@ -233,30 +304,15 @@ impl<T> Notify for Inner<T> where T: Future {
             let node: &Node<T> = Node::from_id(id);
 
             debug_assert!(node as *const _ as *mut _ != self.stub());
-            let mut curr = node.state.load(Acquire);
 
-            loop {
-                if curr & QUEUED == QUEUED {
-                    // Nothing more to do
-                    return;
-                }
+            let prev = node.state.fetch_or(QUEUED, AcqRel);
 
-                // TODO: Prevent overflow
-                let next = (curr | QUEUED) + 1;
+            if prev & QUEUED == 0 {
+                // Enqueue the task
+                self.enqueue(node as *const _ as *mut _);
 
-                let act = node.state.compare_and_swap(curr, next, AcqRel);
-
-                if curr == act {
-                    // Enqueue the task
-                    self.enqueue(node as *const _ as *mut _);
-
-                    // Notify the parent after the task has been enqueued
-                    self.parent.notify();
-
-                    return;
-                }
-
-                curr = act;
+                // Notify the parent after the task has been enqueued
+                self.parent.notify();
             }
         }
     }
@@ -303,10 +359,11 @@ impl<T> Node<T> {
     }
 }
 
+/// Called from `Notify` impl
 unsafe fn release<T>(node: &Node<T>) {
     let old_state = node.state.fetch_sub(1, AcqRel);
 
-    if old_state != 1 {
+    if (old_state & !QUEUED) != 1 {
         return;
     }
 
--- tests/ready_queue.rs
+++ tests/ready_queue.rs
@@ -36,16 +36,56 @@ fn basic_usage() {
 }
 
 #[test]
+fn resolving_errors() {
+    future::lazy(move || {
+        let mut queue = ReadyQueue::new();
+        let (tx1, rx1) = oneshot::channel();
+        let (tx2, rx2) = oneshot::channel();
+        let (tx3, rx3) = oneshot::channel();
+
+        queue.push(rx1);
+        queue.push(rx2);
+        queue.push(rx3);
+
+        assert!(!queue.poll().unwrap().is_ready());
+
+        drop(tx2);
+
+        assert!(queue.poll().is_err());
+        assert!(!queue.poll().unwrap().is_ready());
+
+        drop(tx1);
+        tx3.send("world2").unwrap();
+
+        assert!(queue.poll().is_err());
+        assert_eq!(Ready(Some("world2")), queue.poll().unwrap());
+        assert!(!queue.poll().unwrap().is_ready());
+
+        Ok::<_, ()>(())
+    }).wait().unwrap();
+}
+
+#[test]
 fn dropping_ready_queue() {
     future::lazy(move || {
         let mut queue = ReadyQueue::new();
-        let (mut tx, rx) = oneshot::channel::<()>();
+        let (mut tx1, rx1) = oneshot::channel::<()>();
+        let (mut tx2, rx2) = oneshot::channel::<()>();
+        let (mut tx3, rx3) = oneshot::channel::<()>();
 
-        queue.push(rx);
+        queue.push(rx1);
+        queue.push(rx2);
+        queue.push(rx3);
 
-        assert!(!tx.poll_cancel().unwrap().is_ready());
+        assert!(!tx1.poll_cancel().unwrap().is_ready());
+        assert!(!tx2.poll_cancel().unwrap().is_ready());
+        assert!(!tx3.poll_cancel().unwrap().is_ready());
+
         drop(queue);
-        assert!(tx.poll_cancel().unwrap().is_ready());
+
+        assert!(tx1.poll_cancel().unwrap().is_ready());
+        assert!(tx2.poll_cancel().unwrap().is_ready());
+        assert!(tx3.poll_cancel().unwrap().is_ready());
 
         Ok::<_, ()>(())
     }).wait().unwrap();
@@ -53,4 +93,42 @@ fn dropping_ready_queue() {
 
 #[test]
 fn stress() {
+    const ITER: usize = 500;
+
+    use std::sync::{Arc, Barrier};
+    use std::thread;
+
+    for i in 0..ITER {
+        let n = (i % 10) + 1;
+
+        let mut queue = ReadyQueue::new();
+        let barrier = Arc::new(Barrier::new(n + 1));
+
+        for num in 0..n {
+            let barrier = barrier.clone();
+            let (tx, rx) = oneshot::channel();
+
+            queue.push(rx);
+
+            thread::spawn(move || {
+                barrier.wait();
+                tx.send(num).unwrap();
+            });
+        }
+
+        barrier.wait();
+
+        let mut rx: Vec<_> = queue.wait()
+            .take(n)
+            .map(|res| res.unwrap())
+            .collect();
+
+        assert_eq!(rx.len(), n);
+
+        rx.sort();
+
+        for num in 0..n {
+            assert_eq!(rx[num], num);
+        }
+    }
 }
