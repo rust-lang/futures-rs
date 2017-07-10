85c8a6332061d7a1199eb7ec01ac7bac78ba2b74
Alex Crichton <alex@alexcrichton.com>
2017-07-10 09:38:39 -0700

    Remove cargo.sh script

--- cargo.sh
+++
@@ -1,8 +0,0 @@
-export PATH=$HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin:$PATH
-export PATH=$HOME/code/rust2/build/x86_64-unknown-linux-gnu/stage2/bin:$PATH
-
-# which rustc
-# rustc -vV
-# which cargo
-# cargo -vV
-cargo "$@"
