9328d425e5fd361b08cf38c608509bb00e737520
Alex Crichton <alex@alexcrichton.com>
2017-08-24 08:08:48 -0700

    Add accessor functions to `Stream::inspect`

--- src/stream/inspect.rs
+++ src/stream/inspect.rs
@@ -20,6 +20,31 @@ pub fn new<S, F>(stream: S, f: F) -> Inspect<S, F>
     }
 }
 
+impl<S: Stream, F> Inspect<S, F> {
+    /// Acquires a reference to the underlying stream that this combinator is
+    /// pulling from.
+    pub fn get_ref(&self) -> &S {
+        &self.stream
+    }
+
+    /// Acquires a mutable reference to the underlying stream that this
+    /// combinator is pulling from.
+    ///
+    /// Note that care must be taken to avoid tampering with the state of the
+    /// stream which may otherwise confuse this combinator.
+    pub fn get_mut(&mut self) -> &mut S {
+        &mut self.stream
+    }
+
+    /// Consumes this combinator, returning the underlying stream.
+    ///
+    /// Note that this may discard intermediate state of this combinator, so
+    /// care should be taken to avoid losing resources when this is called.
+    pub fn into_inner(self) -> S {
+        self.stream
+    }
+}
+
 impl<S, F> Stream for Inspect<S, F>
     where S: Stream,
           F: FnMut(&S::Item),
