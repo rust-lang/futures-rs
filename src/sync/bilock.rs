3f57b1f17375defc9c4838845b6b185d3c5f2bf8
Diggory Blake <diggsey@googlemail.com>
2017-10-12 23:01:19 +0100

    Add comments and clean up code

--- src/sync/bilock.rs
+++ src/sync/bilock.rs
@@ -32,13 +32,118 @@ use task::{self, Task};
 #[derive(Debug)]
 pub struct BiLock<T> {
     inner: Arc<Inner<T>>,
+    // The `index` is set to `true` for one side of the `BiLock` and `false` for
+    // the other side.
     index: bool
 }
 
+/// IMPLEMENTATION DETAILS
+/// ======================
+/// 
+/// Token system
+/// ------------
+/// It is necessary to protect the unsynchronised parts of the data structure
+/// (tasks, value) from concurrent access by both sides of the BiLock.
+/// To achieve this, there are three "tokens" numbered 0, 1, 2. At any given
+/// time, each side of the BiLock owns one of these tokens, and the remaining
+/// token is free. To access an element in the task array or the value, the
+/// corresponding token must be owned:
+///  ___________________
+/// | TOKEN |   FIELD   |
+/// |_______|___________|
+/// |     0 |  tasks[0] |
+/// |     1 |  tasks[1] |
+/// |     2 |   value   |
+/// |_______|___________|
+/// 
+/// If one side of the BiLock owns the token "2", then it has taken the lock.
+/// If the token "2" is free, then neither side has taken the lock.
+/// 
+/// State encoding
+/// --------------
+/// The ownership of the tokens is encoded entirely withing the state field,
+/// which allows it to be updated atomically.
+/// 
+/// The "LEFT" and "RIGHT" columns indicate which token is owned by each side
+/// of the BiLock, and the "FREE" column shows the remaining token:
+///  _________________________________
+/// | state % 6 | LEFT | FREE | RIGHT |
+/// |___________|______|______|_______|
+/// |         0 |    0 |    1 |     2 |
+/// |         1 |    1 |    0 |     2 |
+/// |         2 |    2 |    0 |     1 |
+/// |         3 |    0 |    2 |     1 |
+/// |         4 |    1 |    2 |     0 |
+/// |         5 |    2 |    1 |     0 |
+/// |___________|______|______|_______|
+/// 
+/// Each side of the BiLock can perform a single atomic operation: it can swap
+/// its token with the free token.
+/// 
+/// For the LEFT side, by examining the table above, we can see that this
+/// corresponds to the operation "XOR 1":
+///     0 <=> 1
+///     2 <=> 3
+///     4 <=> 5
+/// 
+/// For the RIGHT side, by the same process we can see that this corresponds
+/// to the operation "ADD 3 (mod 6)":
+///     0 <=> 3
+///     1 <=> 4
+///     2 <=> 5
+/// 
+/// This yields a simple implementation of the swap operation for each side
+/// of the BiLock: `state.fetch_xor(1, AcqRel)`, `state.fetch_add(3, AcqRel)`.
+/// 
+/// The "lock" operation
+/// --------------------
+/// To implement the "lock" operation, we must own token "0" or "1", or else we
+/// would already have the lock. This gives us access to an entry in the
+/// "tasks" array. We store the current task handle here in case we fail
+/// to take the lock and need to be woken up.
+/// 
+/// Once we have stored the task handle, we perform the atomic swap, and
+/// we check what token we got back. If we got back "2" then we succeeded
+/// in taking the lock. Otherwise, we return NotReady, suspending the task.
+/// If this occurs, the "FREE" token now contains our task handle.
+/// 
+/// The "unlock" operation
+/// ----------------------
+/// To implement the "unlock" operation, we must own token "2". We perform
+/// the atomic swap operation, which results in the "FREE" token being "2"
+/// (ie. the lock is not held by anyone). In exchange, we get back either
+/// the "0" or "1" tokens.
+/// 
+/// At this point we must wake up the other side of the BiLock if it's
+/// currently waiting on the lock. Conveniently, the token we just got
+/// back gives us access to any task handle that was stored if the other
+/// side suspended itself.
+/// 
+/// Optimisations
+/// -------------
+/// Obtaining the current task handle can be expensive, so the "lock"
+/// implementation has a fast path which tries to take the lock without
+/// storing a task handle. If this fails it simply tries to take the lock
+/// again, this time storing a task handle. In the event that it fails that
+/// second time, it returns NotReady.
+/// 
+/// Wrapping behaviour
+/// ------------------
+/// Since modular arithmatic is not a supported atomic operation, normal
+/// addition is used in the "ADD 3 (mod 6)" operation. This results in the
+/// `state` growing indefinitely. As a result, it must be protected from
+/// wrapping around as it would naturally at a power of 2 determined by
+/// the machine word size. After performing a lock, the side of the BiLock
+/// which performs this addition checks if state is getting close to this
+/// limit, and if so subtracts a large multiple of 6 to bring it back down
+/// again.
 #[derive(Debug)]
 struct Inner<T> {
+    // (state % 6) identifies a state in a state machine
     state: AtomicUsize,
+    // Up to two tasks may be trying to park themselves concurrently
     tasks: [UnsafeCell<Option<Task>>; 2],
+    // The protected value
     value: Option<UnsafeCell<T>>,
 }
 
@@ -125,51 +230,66 @@ impl<T> Inner<T> {
     }
 
     unsafe fn poll_lock(&self, caller_index: bool) -> bool {
-        let mut state;
+        let mut prev_state;
         if caller_index {
-            // This needs precisely Acquire/Release semantics
-            state = self.state.fetch_add(3, AcqRel);
+            // Swap our token with the "FREE" token
+            prev_state = self.state.fetch_add(3, AcqRel);
 
-            // Slow path if lock failed
-            match state % 6 {
+            // See what happened
+            match prev_state % 6 {
+                // Slow paths if lock failed
                 2 => {
-                    *self.tasks.get_unchecked(0).get() = Some(task::current());
-                    state = self.state.fetch_add(3, AcqRel);
+                    // Store our task handle
+                    *self.tasks[0].get() = Some(task::current());
+                    // Try again
+                    prev_state = self.state.fetch_add(3, AcqRel);
                 },
                 5 => {
-                    *self.tasks.get_unchecked(1).get() = Some(task::current());
-                    state = self.state.fetch_add(3, AcqRel);
+                    // Store our task handle
+                    *self.tasks[1].get() = Some(task::current());
+                    // Try again
+                    prev_state = self.state.fetch_add(3, AcqRel);
                 },
+                // If we had the lock originally, we should never have been called!
                 0 | 1 => panic!("Lock already held"),
+                // We obtained the lock!
                 _ => {}
             }
 
             // Make sure the state wraps around at a multiple of 6, and leave plenty of
             // room at the end.
             const UPPER_LIMIT: usize = !0usize - 63;
-            if state >= UPPER_LIMIT {
+            if prev_state >= UPPER_LIMIT {
                 self.state.fetch_sub(UPPER_LIMIT, Relaxed);
             }
         } else {
-            // This needs precisely Acquire/Release semantics
-            state = self.state.fetch_xor(1, AcqRel);
+            // Swap our token with the "FREE" token
+            prev_state = self.state.fetch_xor(1, AcqRel);
 
-            // Slow path if lock failed
-            match state % 6 {
+            // See what happened
+            match prev_state % 6 {
+                // Slow paths if lock failed
                 0 => {
-                    *self.tasks.get_unchecked(1).get() = Some(task::current());
-                    state = self.state.fetch_xor(1, AcqRel);
+                    // Store our task handle
+                    *self.tasks[1].get() = Some(task::current());
+                    // Try again
+                    prev_state = self.state.fetch_xor(1, AcqRel);
                 },
                 1 => {
-                    *self.tasks.get_unchecked(0).get() = Some(task::current());
-                    state = self.state.fetch_xor(1, AcqRel);
+                    // Store our task handle
+                    *self.tasks[0].get() = Some(task::current());
+                    // Try again
+                    prev_state = self.state.fetch_xor(1, AcqRel);
                 },
+                // If we had the lock originally, we should never have been called!
                 2 | 5 => panic!("Lock already held"),
+                // We obtained the lock!
                 _ => {}
             }
         }
 
-        match state % 6 {
+        // Return true if we obtained the lock
+        match prev_state % 6 {
             3 | 4 => true,
             _ => false
         }
@@ -177,25 +297,25 @@ impl<T> Inner<T> {
 
     unsafe fn unlock(&self, caller_index: bool) {
         if caller_index {
-            // This needs precisely Acquire/Release semantics
-            let state = self.state.fetch_add(3, AcqRel);
+            // Swap our token with the "FREE" token
+            let prev_state = self.state.fetch_add(3, AcqRel);
 
             // Wake up a waiting task if necessary
-            match state % 6 {
-                0 => (*self.tasks.get_unchecked(1).get()).take().map(|t| t.notify()),
-                1 => (*self.tasks.get_unchecked(0).get()).take().map(|t| t.notify()),
+            (*match prev_state % 6 {
+                0 => self.tasks[1].get(),
+                1 => self.tasks[0].get(),
                 _ => panic!("Lock not held")
-            };
+            }).take().map(|t| t.notify());
         } else {
-            // This needs precisely Acquire/Release semantics
-            let state = self.state.fetch_xor(1, AcqRel);
+            // Swap our token with the "FREE" token
+            let prev_state = self.state.fetch_xor(1, AcqRel);
 
             // Wake up a waiting task if necessary
-            match state % 6 {
-                2 => (*self.tasks.get_unchecked(0).get()).take().map(|t| t.notify()),
-                5 => (*self.tasks.get_unchecked(1).get()).take().map(|t| t.notify()),
+            (*match prev_state % 6 {
+                2 => self.tasks[0].get(),
+                5 => self.tasks[1].get(),
                 _ => panic!("Lock not held")
-            };
+            }).take().map(|t| t.notify());
         }
     }
 
