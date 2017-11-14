59fb3ced5a33deec626157ecaca6566d14ff66c9
Carl Lerche <me@carllerche.com>
2017-11-14 14:24:27 -0800

    Add test for blocking non-daemon tasks

--- tests/current_thread_executor.rs
+++ tests/current_thread_executor.rs
@@ -1,5 +1,6 @@
 extern crate futures;
 
+use futures::Future;
 use futures::future::lazy;
 use futures::executor::CurrentThread;
 
@@ -18,6 +19,33 @@ fn spawning_from_init_future() {
         let cnt = cnt.clone();
 
         CurrentThread::spawn(lazy(move || {
+            cnt.set(1 + cnt.get());
+            Ok(())
+        }));
+    });
+
+    assert_eq!(1, cnt.get());
+}
+
+#[test]
+fn block_waits_for_non_daemon() {
+    use futures::sync::oneshot;
+    use std::thread;
+    use std::time::Duration;
+
+    let cnt = Rc::new(Cell::new(0));
+
+    CurrentThread::block_with_init(|| {
+        let cnt = cnt.clone();
+
+        let (tx, rx) = oneshot::channel();
+
+        thread::spawn(|| {
+            thread::sleep(Duration::from_millis(1000));
+            tx.send(()).unwrap();
+        });
+
+        CurrentThread::spawn(rx.then(move |_| {
             cnt.set(1 + cnt.get());
             Ok(())
         }));
