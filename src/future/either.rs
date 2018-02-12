0da26ae56ed71350bf05e734d1487ba5c9562280
Alex Crichton <alex@alexcrichton.com>
2018-02-12 13:47:32 -0800

    Implement Stream for Either
    Closes #614

--- src/future/either.rs
+++ src/future/either.rs
@@ -1,4 +1,4 @@
-use {Future, Poll};
+use {Future, Poll, Stream};
 
 /// Combines two different futures yielding the same item and error
 /// types into a single type.
@@ -31,6 +31,21 @@ impl<A, B> Future for Either<A, B>
     type Error = A::Error;
 
     fn poll(&mut self) -> Poll<A::Item, A::Error> {
+        match *self {
+            Either::A(ref mut a) => a.poll(),
+            Either::B(ref mut b) => b.poll(),
+        }
+    }
+}
+
+impl<A, B> Stream for Either<A, B>
+    where A: Stream,
+          B: Stream<Item = A::Item, Error = A::Error>
+{
+    type Item = A::Item;
+    type Error = A::Error;
+
+    fn poll(&mut self) -> Poll<Option<A::Item>, A::Error> {
         match *self {
             Either::A(ref mut a) => a.poll(),
             Either::B(ref mut b) => b.poll(),
