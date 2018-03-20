0664d74aa49ad5cefa255234977deedebd042611
Aaron Turon <aturon@mozilla.com>
2018-03-20 12:46:30 -0700

    Generalize `once` to use `IntoFuture`
    Fixes #826

--- futures-util/src/stream/once.rs
+++ futures-util/src/stream/once.rs
@@ -1,4 +1,4 @@
-use futures_core::{Poll, Async, Stream};
+use futures_core::{Poll, Async, Stream, IntoFuture, Future};
 use futures_core::task;
 
 /// A stream which emits single element and then EOF.
@@ -6,7 +6,7 @@ use futures_core::task;
 /// This stream will never block and is always ready.
 #[derive(Debug)]
 #[must_use = "streams do nothing unless polled"]
-pub struct Once<T, E>(Option<Result<T, E>>);
+pub struct Once<F>(Option<F>);
 
 /// Creates a stream of single element
 ///
@@ -18,28 +18,34 @@ pub struct Once<T, E>(Option<Result<T, E>>);
 /// use futures_executor::block_on;
 ///
 /// # fn main() {
-/// let mut stream = stream::once::<(), _>(Err(17));
+/// let mut stream = stream::once::<Result<(), _>>(Err(17));
 /// let collected: Result<Vec<_>, _> = block_on(stream.collect());
 /// assert_eq!(collected, Err(17));
 ///
-/// let mut stream = stream::once::<_, ()>(Ok(92));
+/// let mut stream = stream::once::<Result<_, ()>>(Ok(92));
 /// let collected: Result<Vec<_>, _> = block_on(stream.collect());
 /// assert_eq!(collected, Ok(vec![92]));
 /// # }
 /// ```
-pub fn once<T, E>(item: Result<T, E>) -> Once<T, E> {
-    Once(Some(item))
+pub fn once<F: IntoFuture>(item: F) -> Once<F::Future> {
+    Once(Some(item.into_future()))
 }
 
-impl<T, E> Stream for Once<T, E> {
-    type Item = T;
-    type Error = E;
+impl<F: Future> Stream for Once<F> {
+    type Item = F::Item;
+    type Error = F::Error;
 
-    fn poll_next(&mut self, _: &mut task::Context) -> Poll<Option<T>, E> {
-        match self.0.take() {
-            Some(Ok(e)) => Ok(Async::Ready(Some(e))),
-            Some(Err(e)) => Err(e),
-            None => Ok(Async::Ready(None)),
+    fn poll_next(&mut self, cx: &mut task::Context) -> Poll<Option<F::Item>, F::Error> {
+        if let Some(mut f) = self.0.take() {
+            match f.poll(cx)? {
+                Async::Ready(x) => Ok(Async::Ready(Some(x))),
+                Async::Pending => {
+                    self.0 = Some(f);
+                    Ok(Async::Pending)
+                }
+            }
+        } else {
+            Ok(Async::Ready(None))
         }
     }
 }
