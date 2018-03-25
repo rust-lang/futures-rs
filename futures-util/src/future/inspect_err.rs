112672cde81d8877b63aca6f38b188e9a501cc95
Wim Looman <wim@nemo157.com>
2018-03-25 23:22:17 +0200

    Add FutureExt::inspect_err method
    Fixes #898

--- futures-util/src/future/inspect_err.rs
+++ futures-util/src/future/inspect_err.rs
@@ -1,29 +1,29 @@
 use futures_core::{Future, Poll, Async};
 use futures_core::task;
 
-/// Do something with the item of a future, passing it on.
+/// Do something with the error of a future, passing it on.
 ///
-/// This is created by the [`FutureExt::inspect`] method.
+/// This is created by the [`FutureExt::inspect_err`] method.
 #[derive(Debug)]
 #[must_use = "futures do nothing unless polled"]
-pub struct Inspect<A, F> where A: Future {
+pub struct InspectErr<A, F> where A: Future {
     future: A,
     f: Option<F>,
 }
 
-pub fn new<A, F>(future: A, f: F) -> Inspect<A, F>
+pub fn new<A, F>(future: A, f: F) -> InspectErr<A, F>
     where A: Future,
-          F: FnOnce(&A::Item),
+          F: FnOnce(&A::Error),
 {
-    Inspect {
+    InspectErr {
         future: future,
         f: Some(f),
     }
 }
 
-impl<A, F> Future for Inspect<A, F>
+impl<A, F> Future for InspectErr<A, F>
     where A: Future,
-          F: FnOnce(&A::Item),
+          F: FnOnce(&A::Error),
 {
     type Item = A::Item;
     type Error = A::Error;
@@ -31,11 +31,11 @@ impl<A, F> Future for Inspect<A, F>
     fn poll(&mut self, cx: &mut task::Context) -> Poll<A::Item, A::Error> {
         match self.future.poll(cx) {
             Ok(Async::Pending) => Ok(Async::Pending),
-            Ok(Async::Ready(e)) => {
-                (self.f.take().expect("cannot poll Inspect twice"))(&e);
-                Ok(Async::Ready(e))
+            Ok(Async::Ready(e)) => Ok(Async::Ready(e)),
+            Err(e) => {
+                (self.f.take().expect("cannot poll InspectErr twice"))(&e);
+                Err(e)
             },
-            Err(e) => Err(e),
         }
     }
 }
