dc89c70d0f5336480fa172613c61cc8c1f2ccaf4
Taylor Cramer <cramertj@google.com>
2018-03-27 15:10:57 +0200

    Revert "Replace the Future impl for Option with IntoFuture"
    This reverts commit db7799587562627e03ba011058cb4f02645f76fb.

--- src/future/option.rs
+++ src/future/option.rs
@@ -1,33 +1,13 @@
 //! Definition of the `Option` (optional step) combinator
 
-use {Future, IntoFuture, Poll, Async};
-use core::option;
+use {Future, Poll, Async};
 
-/// An optional future.
-///
-/// Created by `Option::into_future`.
-#[derive(Debug, Clone)]
-#[must_use = "futures do nothing unless polled"]
-pub struct Option<F> {
-    inner: option::Option<F>,
-}
+impl<F, T, E> Future for Option<F> where F: Future<Item=T, Error=E> {
+    type Item = Option<T>;
+    type Error = E;
 
-impl<F> IntoFuture for option::Option<F> where F: Future {
-    type Future = Option<F>;
-    type Item = option::Option<F::Item>;
-    type Error = F::Error;
-
-    fn into_future(self) -> Self::Future {
-        Option { inner: self }
-    }
-}
-
-impl<F> Future for Option<F> where F: Future {
-    type Item = option::Option<F::Item>;
-    type Error = F::Error;
-
-    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-        match self.inner {
+    fn poll(&mut self) -> Poll<Option<T>, E> {
+        match *self {
             None => Ok(Async::Ready(None)),
             Some(ref mut x) => x.poll().map(|x| x.map(Some)),
         }
