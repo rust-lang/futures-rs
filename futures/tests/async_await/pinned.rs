b01540575b319fd14ae7d06add638e7b7add684d
Wim Looman <wim@nemo157.com>
2018-04-03 08:57:55 +0200

    Add test case checking for macro leakage

--- futures/tests/async_await/pinned.rs
+++ futures/tests/async_await/pinned.rs
@@ -21,6 +21,21 @@ fn qux(x: i32) -> Result<i32, i32> {
     await!(baz(x).pin())
 }
 
+// This test is to check that the internal __await macro generated in each
+// function does not leak across function boundaries. If it did then calling
+// the #[async] version of __await in the #[async_move] function should cause
+// a 'borrow may still be in use when generator yields' error, while calling
+// the #[async_move] version in the #[async] function should fail because `bar`
+// does not implement `Future`.
+#[async_move]
+fn qux2(x: i32) -> Result<i32, i32> {
+    #[async]
+    fn baz2(x: i32) -> Result<i32, i32> {
+        await!(bar(&x))
+    }
+    await!(baz2(x).pin())
+}
+
 #[async_stream(item = u64)]
 fn _stream1() -> Result<(), i32> {
     fn integer() -> u64 { 1 }
@@ -46,5 +61,6 @@ fn main() {
     assert_eq!(block_on_stable(bar(&1)), Ok(1));
     assert_eq!(block_on_stable(baz(17)), Ok(17));
     assert_eq!(block_on_stable(qux(17)), Ok(17));
+    assert_eq!(block_on_stable(qux2(17)), Ok(17));
     assert_eq!(block_on_stable(uses_async_for()), Ok(vec![0, 1]));
 }
