f2354292c4b92d97f33fe4c6a9c473a6878d639c
Josef Reinhard Brandl <mail@josefbrandl.de>
2018-06-23 00:59:21 +0200

    Implement `IterMut` in terms of `IterPinMut`

--- futures-util/src/stream/futures_unordered/iter_mut.rs
+++ futures-util/src/stream/futures_unordered/iter_mut.rs
@@ -1,33 +1,21 @@
-use std::marker::{PhantomData, Unpin};
-use super::FuturesUnordered;
-use super::node::Node;
+use std::marker::Unpin;
+use std::mem::PinMut;
+
+use super::iter_pin_mut::IterPinMut;
 
 #[derive(Debug)]
 /// Mutable iterator over all futures in the unordered set.
-pub struct IterMut<'a, F: 'a + Unpin> {
-    pub(super) node: *const Node<F>,
-    pub(super) len: usize,
-    pub(super) _marker: PhantomData<&'a mut FuturesUnordered<F>>
-}
+pub struct IterMut<'a, F: 'a + Unpin> (pub(super) IterPinMut<'a, F>);
 
 impl<'a, F: Unpin> Iterator for IterMut<'a, F> {
     type Item = &'a mut F;
 
     fn next(&mut self) -> Option<&'a mut F> {
-        if self.node.is_null() {
-            return None;
-        }
-        unsafe {
-            let future = (*(*self.node).future.get()).as_mut().unwrap();
-            let next = *(*self.node).next_all.get();
-            self.node = next;
-            self.len -= 1;
-            Some(future)
-        }
+        self.0.next().map(|f| unsafe { PinMut::get_mut(f) })
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        (self.len, Some(self.len))
+        self.0.size_hint()
     }
 }
 
