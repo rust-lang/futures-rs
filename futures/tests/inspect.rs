c156bd83f8c9c1a291b4b93451dbfbeaf280d970
Josef Reinhard Brandl <mail@josefbrandl.de>
2018-07-04 16:41:46 +0200

    `FutureExt::inspect` test

---
+++ futures/tests/inspect.rs
@@ -0,0 +1,17 @@
+extern crate futures;
+
+use futures::executor::block_on;
+use futures::future;
+use futures::prelude::*;
+
+#[test]
+fn smoke() {
+    let mut counter = 0;
+
+    {
+        let work = future::ready::<i32>(40).inspect(|val| { counter += *val; });
+        assert_eq!(block_on(work), 40);
+    }
+
+    assert_eq!(counter, 40);
+}
