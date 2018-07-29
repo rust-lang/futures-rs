be9253f24d4a8bac12e26273e75515fd244e94c8
Taylor Cramer <cramertj@google.com>
2018-07-28 19:00:41 -0700

    Prevent shadowing of future name in select!

--- futures-util/src/async_await/select.rs
+++ futures-util/src/async_await/select.rs
@@ -10,13 +10,23 @@ macro_rules! select {
     )*) => { {
         $(
             $crate::async_await::assert_unpin(&$name);
-            let mut $name = $crate::future::maybe_done(&mut $name);
-            let mut $name = $crate::core_reexport::mem::PinMut::new(&mut $name);
         )*
+        #[allow(bad_style)]
+        struct __priv<$($name,)*> {
+            $(
+                $name: $name,
+            )*
+        }
+        let mut __priv_futures = __priv {
+            $(
+                $name: $crate::future::maybe_done(&mut $name),
+            )*
+        };
         loop {
             $(
                 if let $crate::core_reexport::task::Poll::Ready(()) =
-                    $crate::poll!($name.reborrow())
+                    $crate::poll!($crate::core_reexport::mem::PinMut::new(
+                        &mut __priv_futures.$name))
                 {
                     break;
                 }
@@ -27,7 +37,9 @@ macro_rules! select {
             unreachable!()
         }
         $(
-            else if let Some($name) = $name.take_output() {
+            else if let Some($name) =
+                $crate::core_reexport::mem::PinMut::new(&mut __priv_futures.$name).take_output()
+            {
                 let _ = $name; // suppress "unused" warning for binding name
                 $body
             }
