18abba1b7238bd4e2c4e897f0cdd023f37113ac8
Taylor Cramer <cramertj@google.com>
2018-08-01 17:48:46 -0700

    Add tests to prevent size regressions in async/await macros

--- futures/tests/async_await_macros.rs
+++ futures/tests/async_await_macros.rs
@@ -1,6 +1,6 @@
 #![feature(async_await, await_macro, pin, arbitrary_self_types, futures_api)]
 
-use futures::{Poll, pending, poll, pin_mut, join, select};
+use futures::{Poll, future, pending, poll, pin_mut, join, try_join, select};
 use futures::channel::oneshot;
 use futures::executor::block_on;
 
@@ -74,4 +74,57 @@ fn select_can_move_uncompleted_futures() {
         }
     });
     assert!(ran);
+}
+
+#[test]
+fn select_size() {
+    let fut = async {
+        let mut ready = future::ready(0i32);
+        select! {
+            ready => {},
+        }
+    };
+    assert_eq!(::std::mem::size_of_val(&fut), 40);
+
+    let fut = async {
+        let mut ready1 = future::ready(0i32);
+        let mut ready2 = future::ready(0i32);
+        select! {
+            ready1 => {},
+            ready2 => {},
+        }
+    };
+    assert_eq!(::std::mem::size_of_val(&fut), 56);
+}
+
+#[test]
+fn join_size() {
+    let fut = async {
+        let ready = future::ready(0i32);
+        join!(ready)
+    };
+    assert_eq!(::std::mem::size_of_val(&fut), 48);
+
+    let fut = async {
+        let ready1 = future::ready(0i32);
+        let ready2 = future::ready(0i32);
+        join!(ready1, ready2)
+    };
+    assert_eq!(::std::mem::size_of_val(&fut), 72);
+}
+
+#[test]
+fn try_join_size() {
+    let fut = async {
+        let ready = future::ready(Ok::<i32, i32>(0));
+        try_join!(ready)
+    };
+    assert_eq!(::std::mem::size_of_val(&fut), 48);
+
+    let fut = async {
+        let ready1 = future::ready(Ok::<i32, i32>(0));
+        let ready2 = future::ready(Ok::<i32, i32>(0));
+        try_join!(ready1, ready2)
+    };
+    assert_eq!(::std::mem::size_of_val(&fut), 80);
 }
