8e84a4ab88a00594ed4396d5ff95731327002e6e
Josef Reinhard Brandl <mail@josefbrandl.de>
2018-08-08 15:45:21 +0200

    Change doc URLs to .../futures-api-docs

--- futures-io/src/lib.rs
+++ futures-io/src/lib.rs
@@ -9,7 +9,7 @@
 #![warn(missing_docs, missing_debug_implementations)]
 #![deny(bare_trait_objects)]
 
-#![doc(html_root_url = "https://rust-lang-nursery.github.io/futures-doc/0.3.0-alpha.2/futures_io")]
+#![doc(html_root_url = "https://rust-lang-nursery.github.io/futures-api-docs/0.3.0-alpha.2/futures_io")]
 
 #![feature(futures_api)]
 
