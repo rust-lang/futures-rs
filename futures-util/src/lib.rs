b474caa28ab0e3ed33bcbc0c1135dfeef23b2423
Taylor Cramer <cramertj@google.com>
2018-10-16 12:32:39 -0700

    Stage 0.3.0-alpha.8 release

--- futures-util/src/lib.rs
+++ futures-util/src/lib.rs
@@ -9,7 +9,7 @@
 #![warn(missing_docs, missing_debug_implementations)]
 #![deny(bare_trait_objects)]
 
-#![doc(html_root_url = "https://rust-lang-nursery.github.io/futures-api-docs/0.3.0-alpha.7/futures_util")]
+#![doc(html_root_url = "https://rust-lang-nursery.github.io/futures-api-docs/0.3.0-alpha.8/futures_util")]
 
 macro_rules! if_std {
     ($($i:item)*) => ($(
