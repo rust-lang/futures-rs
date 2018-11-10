d7fffb0347a51a268a5b43916d3583bfba9ec8d5
Stepan Koltsov <stepan.koltsov@gmail.com>
2018-11-10 01:20:03 +0000

    Move AtomicWaker to futures-core
    ... so `AtomicWaker` could be used inside `futures-channel`.
    
    `AtomicWaker` is still accessible to users only from `futures-util`.

--- futures-core/src/task/mod.rs
+++ futures-core/src/task/mod.rs
@@ -1,6 +1,8 @@
 //! Task notification.
 
 mod spawn;
+#[doc(hidden)]
+pub mod __internal;
 pub use self::spawn::{Spawn, LocalSpawn, SpawnError};
 
 pub use core::task::{Poll, Waker, LocalWaker, UnsafeWake};
--- futures-util/src/task/mod.rs
+++ futures-util/src/task/mod.rs
@@ -15,9 +15,4 @@ pub use self::local_waker_ref::{local_waker_ref, local_waker_ref_from_nonlocal, 
     feature = "nightly",
     cfg(all(target_has_atomic = "cas", target_has_atomic = "ptr"))
 )]
-mod atomic_waker;
-#[cfg_attr(
-    feature = "nightly",
-    cfg(all(target_has_atomic = "cas", target_has_atomic = "ptr"))
-)]
-pub use self::atomic_waker::AtomicWaker;
+pub use futures_core::task::__internal::AtomicWaker;
