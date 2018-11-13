2d6f7f051a51471c0a3f248786d6f5dd448ad261
Wim Looman <wim@nemo157.com>
2018-11-13 21:06:21 +0100

    Rename

--- futures-util/tests/select_next_some.rs
+++ futures-util/tests/select_next_some.rs
@@ -7,26 +7,23 @@ use futures_test::future::FutureTestExt;
 
 #[test]
 fn futures_unordered() {
+    // Checks that even though `async_tasks` will yield a `None` and return
+    // `is_terminated() == true` during the first poll, it manages to toggle
+    // back to having items after a future is pushed into it during the second
+    // poll (after pending_once completes).
     futures::executor::block_on(async {
-
         let mut fut = future::ready(1).pending_once();
         let mut async_tasks = FuturesUnordered::new();
         let mut total = 0;
         loop {
             select! {
                 num = fut => {
-                    // First, the `ready` future completes.
                     total += num;
-                    // Then we spawn a new task onto `async_tasks`.
                     async_tasks.push(async { 5 });
                 },
-                // On the next iteration of the loop, the task we spawned
-                // completes.
-                num = async_tasks.next_some() => {
+                num = async_tasks.select_next_some() => {
                     total += num;
                 }
-                // Finally, both the `ready` future and `async_tasks` have
-                // finished, so we enter the `complete` branch.
                 complete => break,
             }
         }
