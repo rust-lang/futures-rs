be4f8bfe3210180f28bde4630e348b44d447264a
Taylor Cramer <cramertj@google.com>
2019-01-14 14:36:02 -0800

    Stage 0.3.0-alpha.12

--- futures-executor/src/lib.rs
+++ futures-executor/src/lib.rs
@@ -7,7 +7,7 @@
 #![warn(missing_docs, missing_debug_implementations)]
 #![deny(bare_trait_objects)]
 
-#![doc(html_root_url = "https://rust-lang-nursery.github.io/futures-api-docs/0.3.0-alpha.11/futures_executor")]
+#![doc(html_root_url = "https://rust-lang-nursery.github.io/futures-api-docs/0.3.0-alpha.12/futures_executor")]
 
 #[cfg(feature = "std")]
 mod local_pool;
