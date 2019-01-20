220aa672528117752af61019de67fcb17f9c1c5a
DCjanus <DCjanus@dcjanus.com>
2019-01-20 13:52:08 +0800

    Add document for 'yield_now!()'

--- futures-util/src/async_await/yield_now.rs
+++ futures-util/src/async_await/yield_now.rs
@@ -3,6 +3,42 @@ use core::pin::Pin;
 use futures_core::future::Future;
 use futures_core::task::{LocalWaker, Poll};
 
+/// A macro which temporarily give up the CPU
+///
+/// # What's the difference between yield_now!() and pending!()?
+///
+/// When using the [`pending!()`](pending!), it must be ensured that [`wake`](std::task::Waker::wake)
+/// is called somewhere when further progress can be made.
+///
+/// `yield_now!()` just like [`std::thread::yield_now`](std::thread::yield_now), but it used for asynchronous programming.
+///
+/// # Examples
+///
+/// ```
+/// #![feature(await_macro, async_await, futures_api)]
+///
+/// use futures::yield_now;
+///
+/// # let mut count = 10usize;
+/// # let mut is_ready = || {
+/// #    count -= 1;
+/// #    count == 0
+/// # };
+/// async {
+///     loop {
+///         if is_ready() {
+///             // finish job
+///             break;
+///         } else {
+///             // do something
+///             yield_now!();
+///         }
+///     }
+/// };
+///
+/// ```
+///
+/// This macro is only usable inside of async functions, closures, and blocks.
 #[macro_export]
 macro_rules! yield_now {
     () => {
