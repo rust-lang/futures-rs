7a8141b039ef3fd8ddd59d69967087d64b362144
Taylor Cramer <cramertj@google.com>
2019-04-26 16:39:21 -0700

    Stage 0.3.0-alpha.15 release

--- futures-channel/src/lib.rs
+++ futures-channel/src/lib.rs
@@ -7,7 +7,7 @@
 
 #![warn(missing_docs, missing_debug_implementations, rust_2018_idioms)]
 
-#![doc(html_root_url = "https://rust-lang-nursery.github.io/futures-api-docs/0.3.0-alpha.14/futures_channel")]
+#![doc(html_root_url = "https://rust-lang-nursery.github.io/futures-api-docs/0.3.0-alpha.15/futures_channel")]
 
 #[cfg(feature = "std")]
 mod lock;
