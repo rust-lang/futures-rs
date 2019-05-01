226c50826fb8356fed2d6ef6e421303d367c11d1
Naja Melan <najamelan@autistici.org>
2019-05-01 16:07:31 +0200

    Some more warnings and links fixed
    TODO: note that paths like futures_core::task::Context actually get redirected to core::task::Context
    in the redered docs. Is this desirable? If so, should we change the paths here?
    
    Also, there is cross crate links in here. They are not going to work anytime soon. Do we put https links
    in here? to here: https://rust-lang-nursery.github.io/futures-api-docs? The problem is these have a
    version hardcoded in the url: 0.3.0-alpha.15 We could link to docs.rs, but currently that says:
    docs.rs failed to build futures-preview-0.3.0-alpha.15 -> ok the reason seems to be that they are on
    2019-04-17 which does still have futures-api unstable feature, so that should get solved.

--- futures-test/src/task/mod.rs
+++ futures-test/src/task/mod.rs
@@ -1,3 +1,12 @@
+// TODO: note that paths like futures_core::task::Context actually get redirected to core::task::Context
+// in the redered docs. Is this desirable? If so, should we change the paths here?
+//
+// Also, there is cross crate links in here. They are not going to work anytime soon. Do we put https links
+// in here? to here: https://rust-lang-nursery.github.io/futures-api-docs? The problem is these have a
+// version hardcoded in the url: 0.3.0-alpha.15 We could link to docs.rs, but currently that says:
+// docs.rs failed to build futures-preview-0.3.0-alpha.15 -> ok the reason seems to be that they are on
+// 2019-04-17 which does still have futures-api unstable feature, so that should get solved.
+//
 //! Task related testing utilities.
 //!
 //! This module provides utilities for creating test
@@ -6,29 +15,29 @@
 //! [`Spawn`](futures_core::task::Spawn) implementations.
 //!
 //! Test contexts:
-//! - [`noop_context`] creates a context that ignores calls to
+//! - [`noop_context`](crate::task::noop_context) creates a context that ignores calls to
 //!   [`cx.waker().wake_by_ref()`](futures_core::task::Waker).
-//! - [`panic_context`] creates a context that panics when
+//! - [`panic_context`](crate::task::panic_context) creates a context that panics when
 //!   [`cx.waker().wake_by_ref()`](futures_core::task::Waker) is called.
 //!
 //! Test wakers:
-//! - [`noop_waker`] creates a waker that ignores calls to
+//! - [`noop_waker`](crate::task::noop_waker) creates a waker that ignores calls to
 //!   [`wake`](futures_core::task::Waker).
-//! - [`panic_waker`] creates a waker that panics when
+//! - [`panic_waker`](crate::task::panic_waker::panic_waker) creates a waker that panics when
 //!   [`wake`](futures_core::task::Waker) is called.
-//! - [`new_count_waker`] creates a waker that increments a counter whenever
+//! - [`new_count_waker`](crate::task::new_count_waker) creates a waker that increments a counter whenever
 //!   [`wake`](futures_core::task::Waker) is called.
 //!
 //! Test spawners:
-//! - [`NoopSpawner`] ignores calls to
-//!   [`spawn`](futures_core::task::Spawn::spawn)
-//! - [`PanicSpawner`] panics if [`spawn`](futures_core::task::Spawn::spawn) is
+//! - [`NoopSpawner`](crate::task::NoopSpawner) ignores calls to
+//!   [`spawn`](::futures_core::task::Spawn::spawn)
+//! - [`PanicSpawner`](crate::task::PanicSpawner) panics if [`spawn`](futures_core::task::Spawn::spawn) is
 //!   called.
-//! - [`RecordSpawner`] records the spawned futures.
+//! - [`RecordSpawner`](crate::task::RecordSpawner) records the spawned futures.
 //!
 //! For convenience there additionally exist various functions that directly
-//! return waker/spawner references: [`noop_waker_ref`],
-//! [`panic_waker_ref`], [`noop_spawner_mut`] and [`panic_spawner_mut`].
+//! return waker/spawner references: [`noop_waker_ref`](crate::task::noop_waker_ref),
+//! [`panic_waker_ref`](crate::task::panic_waker_ref), [`noop_spawner_mut`](crate::task::noop_spawner_mut) and [`panic_spawner_mut`](crate::task::panic_spawner_mut).
 
 mod context;
 pub use self::context::{noop_context, panic_context};
