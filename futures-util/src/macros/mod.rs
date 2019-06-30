39e02cd1495c9cef5f089ef83b0f787138236582
Taiki Endo <te316e89@gmail.com>
2019-06-30 16:30:17 +0900

    Remove futures_util::macros module

--- futures-util/src/macros/mod.rs
+++
@@ -1,9 +0,0 @@
-macro_rules! cfg_target_has_atomic {
-    ($($item:item)*) => {$(
-        #[cfg_attr(
-            feature = "cfg-target-has-atomic",
-            cfg(all(target_has_atomic = "cas", target_has_atomic = "ptr"))
-        )]
-        $item
-    )*};
-}
