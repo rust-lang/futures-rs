db1bafb064d3dc48befbf25b10efc370c1947c8e
Taylor Cramer <cramertj@google.com>
2019-07-03 15:17:13 -0700

    Stage 0.3.0-alpha.17

--- futures-io/src/lib.rs
+++ futures-io/src/lib.rs
@@ -17,7 +17,7 @@
 
 #![doc(test(attr(deny(warnings), allow(dead_code, unused_assignments, unused_variables))))]
 
-#![doc(html_root_url = "https://rust-lang-nursery.github.io/futures-api-docs/0.3.0-alpha.16/futures_io")]
+#![doc(html_root_url = "https://rust-lang-nursery.github.io/futures-api-docs/0.3.0-alpha.17/futures_io")]
 
 #[cfg(feature = "std")]
 mod if_std {
