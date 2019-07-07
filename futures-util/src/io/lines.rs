4b0dbf34296b0747bf6da812fd802768e9c379e6
Taiki Endo <te316e89@gmail.com>
2019-07-07 11:32:00 +0900

    Try to use destruct/enum support

--- futures-util/src/io/lines.rs
+++ futures-util/src/io/lines.rs
@@ -1,22 +1,23 @@
+use super::read_line::read_line_internal;
 use futures_core::stream::Stream;
 use futures_core::task::{Context, Poll};
 use futures_io::AsyncBufRead;
+use pin_project::{pin_project, unsafe_project};
 use std::io;
 use std::mem;
 use std::pin::Pin;
-use super::read_line::read_line_internal;
 
 /// Stream for the [`lines`](super::AsyncBufReadExt::lines) method.
+#[unsafe_project(Unpin)]
 #[derive(Debug)]
 #[must_use = "streams do nothing unless polled"]
 pub struct Lines<R> {
+    #[pin]
     reader: R,
     buf: String,
     bytes: Vec<u8>,
     read: usize,
 }
-
-impl<R: Unpin> Unpin for Lines<R> {}
 
 impl<R: AsyncBufRead> Lines<R> {
     pub(super) fn new(reader: R) -> Self {
@@ -32,9 +33,10 @@ impl<R: AsyncBufRead> Lines<R> {
 impl<R: AsyncBufRead> Stream for Lines<R> {
     type Item = io::Result<String>;
 
+    #[pin_project(self)]
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        let Self { reader, buf, bytes, read } = unsafe { self.get_unchecked_mut() };
-        let reader = unsafe { Pin::new_unchecked(reader) };
+        #[project]
+        let Lines { reader, buf, bytes, read } = self;
         let n = ready!(read_line_internal(reader, buf, bytes, read, cx))?;
         if n == 0 && buf.is_empty() {
             return Poll::Ready(None)
