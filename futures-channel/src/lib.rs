f58c0d704cd4a8c408b8d15b27c7301a488ef78f
Taiki Endo <te316e89@gmail.com>
2019-07-19 05:27:20 +0900

    Support oneshot in no_std environment

--- futures-channel/src/lib.rs
+++ futures-channel/src/lib.rs
@@ -3,8 +3,10 @@
 //! This crate provides channels that can be used to communicate between
 //! asynchronous tasks.
 //!
-//! All items of this library are only available when the `std` feature of this
+//! All items of this library are only available when the `std` or `alloc` feature of this
 //! library is activated, and it is activated by default.
+
+#![cfg_attr(feature = "cfg-target-has-atomic", feature(cfg_target_has_atomic))]
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
@@ -17,9 +19,27 @@
 
 #![doc(html_root_url = "https://rust-lang-nursery.github.io/futures-api-docs/0.3.0-alpha.17/futures_channel")]
 
-#[cfg(feature = "std")]
-mod lock;
-#[cfg(feature = "std")]
-pub mod mpsc;
-#[cfg(feature = "std")]
-pub mod oneshot;
+#[cfg(all(feature = "cfg-target-has-atomic", not(feature = "nightly")))]
+compile_error!("The `cfg-target-has-atomic` feature requires the `nightly` feature as an explicit opt-in to unstable features");
+
+macro_rules! cfg_target_has_atomic {
+    ($($item:item)*) => {$(
+        #[cfg_attr(
+            feature = "cfg-target-has-atomic",
+            cfg(all(target_has_atomic = "cas", target_has_atomic = "ptr"))
+        )]
+        $item
+    )*};
+}
+
+cfg_target_has_atomic! {
+    #[cfg(feature = "alloc")]
+    extern crate alloc;
+
+    #[cfg(feature = "alloc")]
+    mod lock;
+    #[cfg(feature = "std")]
+    pub mod mpsc;
+    #[cfg(feature = "alloc")]
+    pub mod oneshot;
+}
