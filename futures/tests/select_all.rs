2760e4b0afaa2af0f5020c6c63ab234906c63e09
John-John Tedro <udoprog@tedro.se>
2019-07-28 07:57:49 +0200

    Optimize future::select_all by not preserving order

--- futures/tests/select_all.rs
+++ futures/tests/select_all.rs
@@ -1,5 +1,6 @@
 use futures::executor::block_on;
 use futures::future::{ready, select_all};
+use std::collections::HashSet;
 
 #[test]
 fn smoke() {
@@ -9,17 +10,20 @@ fn smoke() {
         ready(3),
     ];
 
+    let mut c = vec![1, 2, 3].into_iter().collect::<HashSet<_>>();
+
     let (i, idx, v) = block_on(select_all(v));
-    assert_eq!(i, 1);
+    assert!(c.remove(&i));
     assert_eq!(idx, 0);
 
     let (i, idx, v) = block_on(select_all(v));
-    assert_eq!(i, 2);
+    assert!(c.remove(&i));
     assert_eq!(idx, 0);
 
     let (i, idx, v) = block_on(select_all(v));
-    assert_eq!(i, 3);
+    assert!(c.remove(&i));
     assert_eq!(idx, 0);
 
+    assert!(c.is_empty());
     assert!(v.is_empty());
 }
