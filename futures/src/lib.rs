c2102e0bd243a31bf678bdae06951c187493a15d
Taiki Endo <te316e89@gmail.com>
2019-08-27 20:21:21 +0900

    Use our own io::{Empy, Repeat, Sink}

--- futures/src/lib.rs
+++ futures/src/lib.rs
@@ -300,10 +300,10 @@ pub mod io {
 
     pub use futures_util::io::{
         AsyncReadExt, AsyncWriteExt, AsyncSeekExt, AsyncBufReadExt, AllowStdIo,
-        BufReader, BufWriter, Chain, Close, CopyInto, CopyBufInto, Flush,
-        IntoSink, Lines, Read, ReadExact, ReadHalf, ReadLine, ReadToEnd,
-        ReadToString, ReadUntil, ReadVectored, Seek, Take, Window, Write,
-        WriteAll, WriteHalf, WriteVectored,
+        BufReader, BufWriter, Chain, Close, CopyInto, CopyBufInto, empty, Empty,
+        Flush, IntoSink, Lines, Read, ReadExact, ReadHalf, ReadLine, ReadToEnd,
+        ReadToString, ReadUntil, ReadVectored, repeat, Repeat, Seek, sink, Sink,
+        Take, Window, Write, WriteAll, WriteHalf, WriteVectored,
     };
 }
 
