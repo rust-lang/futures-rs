5476b3eccb2013e96f83f9393ce0731b7b65d8fc
Drake Tetreault <ekardnt@ekardnt.com>
2019-08-30 14:40:36 -0700

    Minor comment update.

--- futures-util/src/test_support/mod.rs
+++ futures-util/src/test_support/mod.rs
@@ -8,6 +8,8 @@ use std::future::Future;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
+/// Convenience method for tests that calls a future's [poll](std::future::Future::poll())
+/// method with a Context containing a Waker that does nothing.
 pub(crate) fn unwakeable_poll<F: Future + Unpin>(fut: &mut F) -> Poll<F::Output> {
     let noop_waker = noop_waker();
     let mut context = Context::from_waker(&noop_waker);
