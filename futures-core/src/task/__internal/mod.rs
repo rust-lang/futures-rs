916e1a5185cfd3e1a6947d9629f0294f099d996c
Taiki Endo <te316e89@gmail.com>
2019-10-16 16:48:18 +0900

    Fix breakage with cfg-target-has-atomic feature

--- futures-core/src/task/__internal/mod.rs
+++ futures-core/src/task/__internal/mod.rs
@@ -1,10 +1,4 @@
-#[cfg_attr(
-    feature = "cfg-target-has-atomic",
-    cfg(all(target_has_atomic = "cas", target_has_atomic = "ptr"))
-)]
+#[cfg_attr(feature = "cfg-target-has-atomic", cfg(target_has_atomic = "ptr"))]
 mod atomic_waker;
-#[cfg_attr(
-    feature = "cfg-target-has-atomic",
-    cfg(all(target_has_atomic = "cas", target_has_atomic = "ptr"))
-)]
+#[cfg_attr(feature = "cfg-target-has-atomic", cfg(target_has_atomic = "ptr"))]
 pub use self::atomic_waker::AtomicWaker;
