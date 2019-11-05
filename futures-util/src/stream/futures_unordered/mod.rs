6fce3f9f0492f4182d0512dca747fa8e30dbaf2a
Taiki Endo <te316e89@gmail.com>
2019-11-05 18:13:14 +0900

    Revert "Move AtomicWaker from futures-core to futures-task"
    This reverts commit dd664a39a7d3b384c30ecc436cd8279b9c7713fd.

--- futures-util/src/stream/futures_unordered/mod.rs
+++ futures-util/src/stream/futures_unordered/mod.rs
@@ -6,7 +6,8 @@
 use futures_core::future::Future;
 use futures_core::stream::{FusedStream, Stream};
 use futures_core::task::{Context, Poll};
-use futures_task::{FutureObj, LocalFutureObj, AtomicWaker, Spawn, LocalSpawn, SpawnError};
+use futures_task::{FutureObj, LocalFutureObj, Spawn, LocalSpawn, SpawnError};
+use crate::task::AtomicWaker;
 use core::cell::UnsafeCell;
 use core::fmt::{self, Debug};
 use core::iter::FromIterator;
