bf1687fb9e2b89803887ff0544452bb437cb2871
Piotr Chromiec <tworec@golem.network>
2020-03-18 20:12:17 +0100

    review: explicit async/await

--- futures-channel/src/oneshot.rs
+++ futures-channel/src/oneshot.rs
@@ -83,21 +83,22 @@ struct Inner<T> {
 /// ```
 /// use futures::channel::oneshot;
 /// use std::{thread, time::Duration};
-/// 
-/// fn main() {
-///     let (sender, receiver) = oneshot::channel::<i32>();
-/// 
-///     thread::spawn(|| {
-///         println!("THREAD: sleeping zzz...");
-///         thread::sleep(Duration::from_millis(2000));
-///         println!("THREAD: i'm awake! sending.");
-///         sender.send(3).unwrap();
-///     });
-/// 
-///     println!("MAIN: doing some useful stuff; and waiting...");
-/// 
-///     println!("MAIN: got: {:?}", futures::executor::block_on(receiver));
-/// }
+///
+/// let (sender, receiver) = oneshot::channel::<i32>();
+///
+/// thread::spawn(|| {
+///     println!("THREAD: sleeping zzz...");
+///     thread::sleep(Duration::from_millis(1000));
+///     println!("THREAD: i'm awake! sending.");
+///     sender.send(3).unwrap();
+/// });
+///
+/// println!("MAIN: doing some useful stuff");
+///
+/// futures::executor::block_on(async {
+///     println!("MAIN: waiting for msg...");
+///     println!("MAIN: got: {:?}", receiver.await)
+/// });
 /// ```
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
     let inner = Arc::new(Inner::new());
