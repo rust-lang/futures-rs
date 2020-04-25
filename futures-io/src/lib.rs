d0a5c80034d4f6e88a0406832233f5e436a43793
Stjepan Glavina <stjepang@gmail.com>
2020-04-25 20:36:08 +0200

    impl AsyncRead/AsyncWrite for Arc<T>

--- futures-io/src/lib.rs
+++ futures-io/src/lib.rs
@@ -29,6 +29,7 @@ mod if_std {
     use std::io;
     use std::ops::DerefMut;
     use std::pin::Pin;
+    use std::sync::Arc;
     use std::task::{Context, Poll};
 
     // Re-export some types from `std::io` so that users don't have to deal
@@ -347,6 +348,28 @@ mod if_std {
         deref_async_read!();
     }
 
+    impl<T: ?Sized + Unpin> AsyncRead for Arc<T>
+    where
+        for<'a> &'a T: AsyncRead,
+    {
+        #[cfg(feature = "read-initializer")]
+        unsafe fn initializer(&self) -> Initializer {
+            (&**self).initializer()
+        }
+
+        fn poll_read(self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &mut [u8])
+            -> Poll<Result<usize>>
+        {
+            Pin::new(&mut &**self).poll_read(cx, buf)
+        }
+
+        fn poll_read_vectored(self: Pin<&mut Self>, cx: &mut Context<'_>, bufs: &mut [IoSliceMut<'_>])
+            -> Poll<Result<usize>>
+        {
+            Pin::new(&mut &**self).poll_read_vectored(cx, bufs)
+        }
+    }
+
     impl<P> AsyncRead for Pin<P>
     where
         P: DerefMut + Unpin,
@@ -425,6 +448,31 @@ mod if_std {
 
     impl<T: ?Sized + AsyncWrite + Unpin> AsyncWrite for &mut T {
         deref_async_write!();
+    }
+
+    impl<T: ?Sized + Unpin> AsyncWrite for Arc<T>
+    where
+        for<'a> &'a T: AsyncWrite,
+    {
+        fn poll_write(self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &[u8])
+            -> Poll<Result<usize>>
+        {
+            Pin::new(&mut &**self).poll_write(cx, buf)
+        }
+
+        fn poll_write_vectored(self: Pin<&mut Self>, cx: &mut Context<'_>, bufs: &[IoSlice<'_>])
+            -> Poll<Result<usize>>
+        {
+            Pin::new(&mut &**self).poll_write_vectored(cx, bufs)
+        }
+
+        fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<()>> {
+            Pin::new(&mut &**self).poll_flush(cx)
+        }
+
+        fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<()>> {
+            Pin::new(&mut &**self).poll_close(cx)
+        }
     }
 
     impl<P> AsyncWrite for Pin<P>
