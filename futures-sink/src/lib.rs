f7fdf67a2d17c24b47d4646a252ea5cc92676020
Taylor Cramer <cramertj@google.com>
2020-05-08 15:53:53 -0700

    Stage 0.3.5

--- futures-sink/src/lib.rs
+++ futures-sink/src/lib.rs
@@ -11,7 +11,7 @@
 
 #![doc(test(attr(deny(warnings), allow(dead_code, unused_assignments, unused_variables))))]
 
-#![doc(html_root_url = "https://docs.rs/futures-sink/0.3.0")]
+#![doc(html_root_url = "https://docs.rs/futures-sink/0.3.5")]
 
 #[cfg(feature = "alloc")]
 extern crate alloc;
