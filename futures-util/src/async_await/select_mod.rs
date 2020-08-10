f317f6f1a1f52e5a420d59cf5324f9b1a5473af6
taoyu <t1997y@vip.qq.com>
2020-08-10 14:25:49 +0800

    Fix assert condition

--- futures-util/src/async_await/select_mod.rs
+++ futures-util/src/async_await/select_mod.rs
@@ -84,7 +84,7 @@ macro_rules! document_select_macro {
         ///     a_res = async_identity_fn(62).fuse() => a_res + 1,
         ///     b_res = async_identity_fn(13).fuse() => b_res,
         /// };
-        /// assert!(res == 63 || res == 12);
+        /// assert!(res == 63 || res == 13);
         /// # });
         /// ```
         ///
