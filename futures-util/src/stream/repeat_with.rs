81926b860885668900da1c2be00c6d7920223318
zhy <zhy@gmail.com>
2020-12-09 00:47:33 +0800

    Fix typo in doc

--- futures-util/src/stream/repeat_with.rs
+++ futures-util/src/stream/repeat_with.rs
@@ -44,7 +44,7 @@ impl<A, F: FnMut() -> A> FusedStream for RepeatWith<F>
 ///
 /// If the element type of the stream you need implements [`Clone`], and
 /// it is OK to keep the source element in memory, you should instead use
-/// the [`repeat()`] function.
+/// the [`stream.repeat()`] function.
 ///
 /// # Examples
 ///
