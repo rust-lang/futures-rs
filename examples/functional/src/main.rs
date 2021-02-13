ff378ad1356010c4ee0d263e1ace255a1b181937
Taiki Endo <te316e89@gmail.com>
2021-02-13 17:32:05 +0900

    Format examples with rustfmt

--- examples/functional/src/main.rs
+++ examples/functional/src/main.rs
@@ -30,9 +30,7 @@ fn main() {
         // responsible for transmission
         pool.spawn_ok(fut_tx_result);
 
-        let fut_values = rx
-            .map(|v| v * 2)
-            .collect();
+        let fut_values = rx.map(|v| v * 2).collect();
 
         // Use the executor provided to this async block to wait for the
         // future to complete.
