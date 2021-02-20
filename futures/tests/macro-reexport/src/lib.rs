7edd57d696029164e305df1f67cbdaee6a09ea10
Taiki Endo <te316e89@gmail.com>
2021-02-20 21:46:03 +0900

    Start futures 0.4 and futures-core 1.0 development
    - futures-core: 0.3 -> 1.0
    - futures-io: as-is
    - others: 0.3 -> 0.4

--- futures/tests/macro-reexport/src/lib.rs
+++ futures/tests/macro-reexport/src/lib.rs
@@ -1,8 +1,8 @@
 // normal reexport
-pub use futures03::{join, try_join, select, select_biased};
+pub use futures04::{join, try_join, select, select_biased};
 
 // reexport + rename
-pub use futures03::{
+pub use futures04::{
     join as join2, try_join as try_join2,
     select as select2, select_biased as select_biased2,
 };
