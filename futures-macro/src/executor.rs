99c786eec474b3b79c3cfd9ac541ab65a91bb24c
Ibraheem Ahmed <ibrah1440@gmail.com>
2021-05-06 11:38:38 -0400

    Apply suggestions from code review
    Co-authored-by: Taiki Endo <te316e89@gmail.com>

--- futures-macro/src/executor.rs
+++ futures-macro/src/executor.rs
@@ -1,25 +1,29 @@
 use proc_macro::TokenStream;
 use quote::quote;
 
-pub(crate) fn test(_: TokenStream, item: TokenStream) -> TokenStream {
+pub(crate) fn test(args: TokenStream, item: TokenStream) -> TokenStream {
+    if !args.is_empty() {
+        return syn::Error::new_spanned(args, "invalid argument"))
+            .to_compile_error()
+            .into();
+    }
     let mut input = syn::parse_macro_input!(item as syn::ItemFn);
     let attrs = &input.attrs;
     let vis = &input.vis;
     let sig = &mut input.sig;
     let body = &input.block;
 
-    if sig.asyncness.is_none() {
+    if sig.asyncness.take().is_none() {
         return syn::Error::new_spanned(sig.fn_token, "Only async functions are supported")
             .to_compile_error()
             .into();
     }
 
-    sig.asyncness = None;
-
     let gen = quote! {
+        #[::core::prelude::v1::test]
         #(#attrs)*
         #vis #sig {
-            ::futures_test::__private::block_on(async move { #body })
+            ::futures_test::__private::block_on(async move #body)
         }
     };
 
