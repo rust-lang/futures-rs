f6d3eb02e47a98018a76c75d4fe4328ff8b27ec0
ibraheemdev <ibrah1440@gmail.com>
2021-05-07 11:07:23 -0400

    apply suggestions from code review

--- futures/tests/test_macro.rs
+++ futures/tests/test_macro.rs
@@ -1,18 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[futures_test::test]
-    async fn it_works() {
-        let fut = async { true };
-        assert!(fut.await);
+#[futures_test::test]
+async fn it_works() {
+    let fut = async { true };
+    assert!(fut.await);
 
-        let fut = async { false };
-        assert!(!fut.await);
-    }
+    let fut = async { false };
+    assert!(!fut.await);
+}
 
-    #[futures_test::test]
-    #[should_panic]
-    async fn it_is_being_run() {
-        let fut = async { false };
-        assert!(fut.await);
-    }
+#[should_panic]
+#[futures_test::test]
+async fn it_is_being_run() {
+    let fut = async { false };
+    assert!(fut.await);
 }
