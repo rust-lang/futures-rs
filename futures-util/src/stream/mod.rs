5f610f8e286080b6ae31f1285648ccd559b4c3bf
Ivan Leshchenko <leshenko.ivan770@gmail.com>
2021-05-17 19:38:06 +0300

    Implement try_chunks (#2438)

--- futures-util/src/stream/mod.rs
+++ futures-util/src/stream/mod.rs
@@ -63,6 +63,9 @@ pub use self::try_stream::IntoAsyncRead;
 #[cfg(feature = "alloc")]
 pub use self::try_stream::{TryBufferUnordered, TryBuffered};
 
+#[cfg(feature = "alloc")]
+pub use self::try_stream::{TryChunks, TryChunksError};
+
 // Primitive streams
 
 mod iter;
