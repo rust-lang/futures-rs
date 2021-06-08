00afc3b9a26061e46434e7777a7099e5d4b456f1
Taiki Endo <te316e89@gmail.com>
2021-06-09 01:02:08 +0900

    Fix typos (#2449)

--- examples/functional/src/main.rs
+++ examples/functional/src/main.rs
@@ -38,7 +38,7 @@ fn main() {
     };
 
     // Actually execute the above future, which will invoke Future::poll and
-    // subsequenty chain appropriate Future::poll and methods needing executors
+    // subsequently chain appropriate Future::poll and methods needing executors
     // to drive all futures. Eventually fut_values will be driven to completion.
     let values: Vec<i32> = executor::block_on(fut_values);
 
