03896d03a888f337427b674bb548236e08df14e7
Taiki Endo <te316e89@gmail.com>
2021-11-23 09:08:57 +0900

    Remove dependency on proc-macro-hack

--- futures-util/build.rs
+++ futures-util/build.rs
@@ -1,6 +1,5 @@
 #![warn(rust_2018_idioms, single_use_lifetimes)]
 
-use autocfg::AutoCfg;
 use std::env;
 
 include!("no_atomic_cas.rs");
@@ -37,24 +36,6 @@ fn main() {
     // don't run the build script.
     if NO_ATOMIC_CAS_TARGETS.contains(&&*target) {
         println!("cargo:rustc-cfg=futures_no_atomic_cas");
-    }
-
-    let cfg = match AutoCfg::new() {
-        Ok(cfg) => cfg,
-        Err(e) => {
-            println!(
-                "cargo:warning={}: unable to determine rustc version: {}",
-                env!("CARGO_PKG_NAME"),
-                e
-            );
-            return;
-        }
-    };
-
-    // Function like procedural macros in expressions patterns statements stabilized in Rust 1.45:
-    // https://blog.rust-lang.org/2020/07/16/Rust-1.45.0.html#stabilizing-function-like-procedural-macros-in-expressions-patterns-and-statements
-    if cfg.probe_rustc_version(1, 45) {
-        println!("cargo:rustc-cfg=fn_like_proc_macro");
     }
 
     println!("cargo:rerun-if-changed=no_atomic_cas.rs");
