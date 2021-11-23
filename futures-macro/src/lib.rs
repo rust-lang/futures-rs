9be41a2f8a04aa516750c5b010074a74bca66e64
Taiki Endo <te316e89@gmail.com>
2021-11-23 09:08:57 +0900

    Remove dependency on proc-macro-hack

--- futures-macro/src/lib.rs
+++ futures-macro/src/lib.rs
@@ -22,34 +22,30 @@ mod select;
 mod stream_select;
 
 /// The `join!` macro.
-#[cfg_attr(fn_like_proc_macro, proc_macro)]
-#[cfg_attr(not(fn_like_proc_macro), proc_macro_hack::proc_macro_hack)]
+#[proc_macro]
 pub fn join_internal(input: TokenStream) -> TokenStream {
     crate::join::join(input)
 }
 
 /// The `try_join!` macro.
-#[cfg_attr(fn_like_proc_macro, proc_macro)]
-#[cfg_attr(not(fn_like_proc_macro), proc_macro_hack::proc_macro_hack)]
+#[proc_macro]
 pub fn try_join_internal(input: TokenStream) -> TokenStream {
     crate::join::try_join(input)
 }
 
 /// The `select!` macro.
-#[cfg_attr(fn_like_proc_macro, proc_macro)]
-#[cfg_attr(not(fn_like_proc_macro), proc_macro_hack::proc_macro_hack)]
+#[proc_macro]
 pub fn select_internal(input: TokenStream) -> TokenStream {
     crate::select::select(input)
 }
 
 /// The `select_biased!` macro.
-#[cfg_attr(fn_like_proc_macro, proc_macro)]
-#[cfg_attr(not(fn_like_proc_macro), proc_macro_hack::proc_macro_hack)]
+#[proc_macro]
 pub fn select_biased_internal(input: TokenStream) -> TokenStream {
     crate::select::select_biased(input)
 }
 
-// TODO: Change this to doc comment once rustdoc bug fixed.
+// TODO: Change this to doc comment once rustdoc bug fixed: https://github.com/rust-lang/futures-rs/pull/2435
 // The `test` attribute.
 #[proc_macro_attribute]
 pub fn test_internal(input: TokenStream, item: TokenStream) -> TokenStream {
@@ -57,8 +53,7 @@ pub fn test_internal(input: TokenStream, item: TokenStream) -> TokenStream {
 }
 
 /// The `stream_select!` macro.
-#[cfg_attr(fn_like_proc_macro, proc_macro)]
-#[cfg_attr(not(fn_like_proc_macro), proc_macro_hack::proc_macro_hack)]
+#[proc_macro]
 pub fn stream_select_internal(input: TokenStream) -> TokenStream {
     crate::stream_select::stream_select(input.into())
         .unwrap_or_else(syn::Error::into_compile_error)
