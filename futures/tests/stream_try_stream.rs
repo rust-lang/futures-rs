f415babc6e79ce80abb5d71130bca69aadf4f189
olegnn <olegnosov1@gmail.com>
2022-03-12 22:40:27 +0400

    Improve test

--- futures/tests/stream_try_stream.rs
+++ futures/tests/stream_try_stream.rs
@@ -42,14 +42,14 @@ fn try_take_while_after_err() {
 
 #[test]
 fn try_flatten_unordered() {
-    let s = stream::iter(1..5)
+    let s = stream::iter(1..7)
         .map(|val: u32| {
             if val % 2 == 0 {
                 Ok(stream::unfold((val, 1), |(val, pow)| async move {
                     Some((val.pow(pow), (val, pow + 1)))
                 })
                 .take(3)
-                .map(move |val| if val % 2 == 0 { Ok(val) } else { Err(val) }))
+                .map(move |val| if val % 16 != 0 { Ok(val) } else { Err(val) }))
             } else {
                 Err(val)
             }
@@ -58,7 +58,22 @@ fn try_flatten_unordered() {
 
     block_on(async move {
         assert_eq!(
-            vec![Err(1), Ok(2), Err(3), Ok(4), Ok(4), Ok(16), Ok(8), Ok(64)],
+            // All numbers can be divided by 16 and odds must be `Err`
+            // For all basic evens we must have powers from 1 to 3
+            vec![
+                Err(1),
+                Ok(2),
+                Err(3),
+                Ok(4),
+                Err(5),
+                Ok(6),
+                Ok(4),
+                Err(16),
+                Ok(36),
+                Ok(8),
+                Err(64),
+                Ok(216)
+            ],
             s.collect::<Vec<_>>().await
         )
     })
