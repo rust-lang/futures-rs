7da477c4a84ad5c44000fee4a8cd9a2237014f13
jefftt <jeff@oxio.io>
2022-06-05 09:16:39 -0400

    stream: Fix Chunk adapters size hints

--- futures-util/src/stream/try_stream/try_chunks.rs
+++ futures-util/src/stream/try_stream/try_chunks.rs
@@ -84,7 +84,7 @@ impl<St: TryStream> Stream for TryChunks<St> {
     fn size_hint(&self) -> (usize, Option<usize>) {
         let chunk_len = if self.items.is_empty() { 0 } else { 1 };
         let (lower, upper) = self.stream.size_hint();
-        let lower = lower.saturating_add(chunk_len);
+        let lower = (lower / self.cap).saturating_add(chunk_len);
         let upper = match upper {
             Some(x) => x.checked_add(chunk_len),
             None => None,
