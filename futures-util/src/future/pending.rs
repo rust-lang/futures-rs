1e8a1dea4ce953058a2ced9aa74559af7063f456
Daniel Henry-Mantilla <daniel.henry.mantilla@gmail.com>
2022-06-13 18:15:03 +0200

    `doc(alias)` `pending()` to `never`
    It can be legitimately to look for the future that _never_ resolves using the name `never`. This PR ensures `pending()` shows up when making such queries.
    
    cc @conradludgate

--- futures-util/src/future/pending.rs
+++ futures-util/src/future/pending.rs
@@ -33,6 +33,7 @@ impl<T> FusedFuture for Pending<T> {
 /// unreachable!();
 /// # });
 /// ```
+#[doc(alias = "never")]
 pub fn pending<T>() -> Pending<T> {
     assert_future::<T, _>(Pending { _data: marker::PhantomData })
 }
