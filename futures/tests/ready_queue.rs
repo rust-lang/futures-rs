3e99d52be71adabaf2deb6ed0fbc4745d1eb48fa
Taiki Endo <te316e89@gmail.com>
2022-07-22 22:14:05 +0900

    Remove unnecessarily allowed lints

--- futures/tests/ready_queue.rs
+++ futures/tests/ready_queue.rs
@@ -1,5 +1,3 @@
-#![allow(clippy::needless_borrow)] // https://github.com/rust-lang/rust-clippy/issues/8367
-
 use futures::channel::oneshot;
 use futures::executor::{block_on, block_on_stream};
 use futures::future;
