09d54ebc029c50b4ce7461eb21e299abe9235ffa
arcnmx <arcnmx@users.noreply.github.com>
2022-12-18 17:49:23 -0800

    impl FusedStream for Buffered (#2676)

--- futures-util/src/stream/stream/buffered.rs
+++ futures-util/src/stream/stream/buffered.rs
@@ -1,4 +1,4 @@
-use crate::stream::{Fuse, FuturesOrdered, StreamExt};
+use crate::stream::{Fuse, FusedStream, FuturesOrdered, StreamExt};
 use core::fmt;
 use core::num::NonZeroUsize;
 use core::pin::Pin;
@@ -97,6 +97,16 @@ where
             None => None,
         };
         (lower, upper)
+    }
+}
+
+impl<St> FusedStream for Buffered<St>
+where
+    St: Stream,
+    St::Item: Future,
+{
+    fn is_terminated(&self) -> bool {
+        self.stream.is_done() && self.in_progress_queue.is_terminated()
     }
 }
 
