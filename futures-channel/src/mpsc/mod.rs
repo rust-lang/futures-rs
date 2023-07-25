247e1f083fff90ad09d433425de5e5d85656fb94
thement <40525767+thement@users.noreply.github.com>
2023-07-25 14:40:09 +0200

    Add `len`, `is_empty` methods for `UnboundedSender` (#2750)
    - add `len`, `is_empty` methods to inspect how many messages are
      enqueued in the message queue.
    - add test for `len` and `is_empty`
    
    Co-authored-by: Jakub Horak <jakub.horak@ibawizard.net>

--- futures-channel/src/mpsc/mod.rs
+++ futures-channel/src/mpsc/mod.rs
@@ -842,6 +842,20 @@ impl<T> UnboundedSender<T> {
         let ptr = self.0.as_ref().map(|inner| inner.ptr());
         ptr.hash(hasher);
     }
+
+    /// Return the number of messages in the queue or 0 if channel is disconnected.
+    pub fn len(&self) -> usize {
+        if let Some(sender) = &self.0 {
+            decode_state(sender.inner.state.load(SeqCst)).num_messages
+        } else {
+            0
+        }
+    }
+
+    /// Return false is channel has no queued messages, true otherwise.
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
 }
 
 impl<T> Clone for Sender<T> {
