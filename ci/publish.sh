f75c4c1cdefe57a6a6a5301c2c18172e2b27f5b2
Taiki Endo <te316e89@gmail.com>
2023-10-26 23:24:13 +0900

    Update ci/publish.sh

--- ci/publish.sh
+++ ci/publish.sh
@@ -2,10 +2,11 @@
 
 set -euo pipefail
 IFS=$'\n\t'
+cd "$(dirname "$0")"/..
 
 # A list of paths to the crate to be published.
 # It will be published in the order listed.
-MEMBERS=(
+members=(
     "futures-core"
     "futures-io"
     "futures-sink"
@@ -18,16 +19,10 @@ MEMBERS=(
     "futures-test"
 )
 
-cd "$(cd "$(dirname "$0")" && pwd)"/..
-
-set -x
-
-for i in "${!MEMBERS[@]}"; do
+for member in "${members[@]}"; do
     (
-        cd "${MEMBERS[${i}]}"
+        set -x
+        cd "${member}"
         cargo +stable publish
     )
-    if [[ $((i + 1)) != "${#MEMBERS[@]}" ]]; then
-        sleep 45
-    fi
 done
