a40204d698c0f435fe53e4d6a3191239375c9103
Taiki Endo <te316e89@gmail.com>
2023-10-26 22:43:29 +0900

    Provide AtomicWaker if portable-atomic feature is enabled, even if atomic CAS is not available

--- futures-core/src/task/__internal/mod.rs
+++ futures-core/src/task/__internal/mod.rs
@@ -1,4 +1,4 @@
-#[cfg(not(futures_no_atomic_cas))]
+#[cfg(any(not(futures_no_atomic_cas), feature = "portable-atomic"))]
 mod atomic_waker;
-#[cfg(not(futures_no_atomic_cas))]
+#[cfg(any(not(futures_no_atomic_cas), feature = "portable-atomic"))]
 pub use self::atomic_waker::AtomicWaker;
