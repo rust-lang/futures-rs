272a3c709f7502df32a70c621ffe02e1e24ca5a4
Taiki Endo <te316e89@gmail.com>
2023-12-08 01:41:53 +0900

    Use cfg(target_has_atomic) on no-std targets

--- futures-core/src/task/__internal/mod.rs
+++ futures-core/src/task/__internal/mod.rs
@@ -1,4 +1,7 @@
-#[cfg(any(not(futures_no_atomic_cas), feature = "portable-atomic"))]
+#[cfg_attr(target_os = "none", cfg(any(target_has_atomic = "ptr", feature = "portable-atomic")))]
 mod atomic_waker;
-#[cfg(any(not(futures_no_atomic_cas), feature = "portable-atomic"))]
+#[cfg_attr(
+    target_os = "none",
+    cfg(any(target_has_atomic = "ptr", feature = "portable-atomic"))
+)]
 pub use self::atomic_waker::AtomicWaker;
