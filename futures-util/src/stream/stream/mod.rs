d7f97c5fbd59f160d4fa8b09010d3d854c8d7b45
nanoqsh <nanoqsh@gmail.com>
2024-05-04 17:49:35 +0500

    Impl `MapWhile`

--- futures-util/src/stream/stream/mod.rs
+++ futures-util/src/stream/stream/mod.rs
@@ -128,6 +128,9 @@ pub use self::take::Take;
 mod take_while;
 pub use self::take_while::TakeWhile;
 
+mod map_while;
+pub use self::map_while::MapWhile;
+
 mod take_until;
 pub use self::take_until::TakeUntil;
 
@@ -992,6 +995,31 @@ pub trait StreamExt: Stream {
         Self: Sized,
     {
         assert_stream::<Self::Item, _>(TakeWhile::new(self, f))
+    }
+
+    /// todo
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # futures::executor::block_on(async {
+    /// use futures::future;
+    /// use futures::stream::{self, StreamExt};
+    ///
+    /// let stream = stream::iter(vec![1, 4, 0, 2]);
+    ///
+    /// let stream = stream.map_while(|x| future::ready(u32::checked_div(16, x)));
+    ///
+    /// assert_eq!(vec![16, 4], stream.collect::<Vec<_>>().await);
+    /// # });
+    /// ```
+    fn map_while<Fut, F, T>(self, f: F) -> MapWhile<Self, Fut, F>
+    where
+        F: FnMut(Self::Item) -> Fut,
+        Fut: Future<Output = Option<T>>,
+        Self: Sized,
+    {
+        assert_stream::<T, _>(MapWhile::new(self, f))
     }
 
     /// Take elements from this stream until the provided future resolves.
