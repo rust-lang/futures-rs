f4d48964574f31ee115c000c0d8c525cc0c9bb1d
@brodycj - C. Jonathan Brody <cj@brodycj.com>
2025-01-01 22:41:56 -0500

    resolve unnecessary map_or Clippy issue
    (as suggested by Clippy nightly)

--- futures/tests/no-std/build.rs
+++ futures/tests/no-std/build.rs
@@ -11,5 +11,5 @@ fn is_nightly() -> bool {
     env::var_os("RUSTC")
         .and_then(|rustc| Command::new(rustc).arg("--version").output().ok())
         .and_then(|output| String::from_utf8(output.stdout).ok())
-        .map_or(false, |version| version.contains("nightly") || version.contains("dev"))
+        .is_some_and(|version| version.contains("nightly") || version.contains("dev"))
 }
