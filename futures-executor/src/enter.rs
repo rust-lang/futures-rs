bbaa0e34e4ab27d89f636ae0c00e2d5745ce4eaf
Taiki Endo <te316e89@gmail.com>
2025-01-13 01:02:59 +0900

    Use const thread_local

--- futures-executor/src/enter.rs
+++ futures-executor/src/enter.rs
@@ -1,7 +1,7 @@
 use std::cell::Cell;
 use std::fmt;
 
-std::thread_local!(static ENTERED: Cell<bool> = Cell::new(false));
+std::thread_local!(static ENTERED: Cell<bool> = const { Cell::new(false) });
 
 /// Represents an executor context.
 ///
