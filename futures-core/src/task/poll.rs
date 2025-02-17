c9b796cc61a8c781a869b2501abaa73085e11144
tison <wander4096@gmail.com>
2025-02-17 23:50:27 +0800

    chore: recommend to use core lib's ready macro (#2925)
    Signed-off-by: tison <wander4096@gmail.com>

--- futures-core/src/task/poll.rs
+++ futures-core/src/task/poll.rs
@@ -1,6 +1,9 @@
-/// Extracts the successful type of a `Poll<T>`.
+/// Extracts the successful type of `Poll<T>`.
 ///
 /// This macro bakes in propagation of `Pending` signals by returning early.
+///
+/// **Note:** Since Rust 1.64, this macro is soft-deprecated in favor of
+/// [`ready!`](core::task::ready) macro in the standard library.
 #[macro_export]
 macro_rules! ready {
     ($e:expr $(,)?) => {
