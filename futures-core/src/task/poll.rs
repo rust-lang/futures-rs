9e8e302af67bb4eb9ef399269dfc323340facd40
tison <wander4096@gmail.com>
2025-02-17 23:05:10 +0800

    address comment
    Signed-off-by: tison <wander4096@gmail.com>

--- futures-core/src/task/poll.rs
+++ futures-core/src/task/poll.rs
@@ -4,10 +4,6 @@
 ///
 /// **Note:** Since Rust 1.64, this macro is soft-deprecated in favor of
 /// [`ready!`](core::task::ready) macro in the standard library.
-#[deprecated(
-    since = "0.4.0",
-    note = "use core::task::ready macro in the standard library instead"
-)]
 #[macro_export]
 macro_rules! ready {
     ($e:expr $(,)?) => {
