f127c611b8a7ac6dcad10d6530413bf58bc4cc53
tison <wander4096@gmail.com>
2025-04-05 17:44:15 +0800

    chore: replace all test usage of pin_mut (#2942)
    Signed-off-by: tison <wander4096@gmail.com>

--- futures/tests/future_join_all.rs
+++ futures/tests/future_join_all.rs
@@ -1,15 +1,14 @@
 use futures::executor::block_on;
 use futures::future::{join_all, ready, Future, JoinAll};
-use futures::pin_mut;
 use std::fmt::Debug;
+use std::pin::pin;
 
 #[track_caller]
 fn assert_done<T>(actual_fut: impl Future<Output = T>, expected: T)
 where
     T: PartialEq + Debug,
 {
-    pin_mut!(actual_fut);
-    let output = block_on(actual_fut);
+    let output = block_on(pin!(actual_fut));
     assert_eq!(output, expected);
 }
 
