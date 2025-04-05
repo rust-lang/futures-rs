8bed711b8a8c0fc438189cf125f2799969e2e4cc
tison <wander4096@gmail.com>
2025-04-05 17:52:26 +0800

    chore: replace all usage of pin_mut
    Signed-off-by: tison <wander4096@gmail.com>

--- futures-util/src/future/future/mod.rs
+++ futures-util/src/future/future/mod.rs
@@ -3,15 +3,14 @@
 //! This module contains a number of functions for working with `Future`s,
 //! including the `FutureExt` trait which adds methods to `Future` types.
 
-#[cfg(feature = "alloc")]
-use alloc::boxed::Box;
-use core::pin::Pin;
-
 use crate::fns::{inspect_fn, into_fn, ok_fn, InspectFn, IntoFn, OkFn};
 use crate::future::{assert_future, Either};
 use crate::never::Never;
-use crate::pin_mut;
 use crate::stream::assert_stream;
+#[cfg(feature = "alloc")]
+use alloc::boxed::Box;
+use core::pin::pin;
+use core::pin::Pin;
 #[cfg(feature = "alloc")]
 use futures_core::future::{BoxFuture, LocalBoxFuture};
 use futures_core::{
@@ -595,8 +594,7 @@ pub trait FutureExt: Future {
         let noop_waker = crate::task::noop_waker();
         let mut cx = Context::from_waker(&noop_waker);
 
-        let this = self;
-        pin_mut!(this);
+        let this = pin!(self);
         match this.poll(&mut cx) {
             Poll::Ready(x) => Some(x),
             _ => None,
